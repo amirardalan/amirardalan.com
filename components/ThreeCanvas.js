6d72eca11960577dc5ee489e2751aeea08a53864
Amir Ardalan <amirrardalan@gmail.com>
2021-05-02 19:30:46 -0700

    Adjust Three camera angle

--- components/ThreeCanvas.js
+++ components/ThreeCanvas.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, Suspense } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { softShadows, MeshWobbleMaterial, OrbitControls } from '@react-three/drei'
 import { useMediaQuery } from '../utils/mediaQuery'
@@ -6,49 +6,16 @@ import { useMediaQuery } from '../utils/mediaQuery'
 // Three.js Canvas
 export default function ThreeCanvas() {
 
+  // Set the breakpoint for Orbital Controls rendering (disable on mobile)
+  const isBreakpoint = useMediaQuery(1024)
+
   // Soft Shadows
   softShadows();
-
-  // Box Mesh
-  function Box(props) {
-    // This reference will give us direct access to the mesh
-    const mesh = useRef()
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-    // Rotate mesh every frame, this is outside of React without overhead
-
-    useFrame(() => (
-      mesh.current.rotation.x += 0.01,
-      mesh.current.rotation.y += 0.01
-    ))
-
-    return (
-      <mesh
-        {...props}
-        ref={mesh}
-        castShadow
-        scale={active ? 1.5 : 1}
-        onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}>
-        <boxBufferGeometry attach='geometry' />
-        <meshStandardMaterial color={hovered ? 'darksalmon' : 'orange'} />
-        <MeshWobbleMaterial
-          color={hovered ? 'darksalmon' : 'orange'}
-          speed='1'
-          attach='material'
-          factor={0.6}
-        />
-      </mesh>
-    )
-  }
 
   // Torus Mesh
   function Torus(props) {
 
     const mesh = useRef()
-
     useFrame(() => (
       mesh.current.rotation.x += 0.002,
       mesh.current.rotation.y += 0.002
@@ -82,47 +49,53 @@ export default function ThreeCanvas() {
         ref={mesh}
         castShadow>
         <sphereGeometry args={[1, 100, 20, 100]} />
-        <meshStandardMaterial color='hotpink' />
+        <meshStandardMaterial color='hotpink' wireframe="true"/>
       </mesh>
     )
   }
-  
-  // Set the breakpoint for Orbital Controls rendering (disable on mobile)
-  const isBreakpoint = useMediaQuery(1024)
 
   return (
-    <Canvas shadowMap shadows colorManagement camera={{ position: [-4, 2, 10], fov: 60 }}>
-      // Lighting
-      <ambientLight intensity={0.3} />
-      <directionalLight
-        castShadow
-        position={[0, 10, 0]}
-        intensity={1.5}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={50}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
-      />
-      <pointLight position={[-10, 0, -20]} />
-      <pointLight castShadow position={[0, -10, 0]} />
-      <group>
-        // Floor mesh
-        <mesh
-          receiveShadow
-          rotation={[-Math.PI / 2, 0, 0]}
-          position={[0, -3, 0]}>
-          <planeBufferGeometry attach='geometry' args={[100, 100]} />
-          {/* <meshStandardMaterial attach='material' color={'yellow'} /> */}
-          <shadowMaterial attach='material' opacity={0.3} />
-        </mesh>
-        // Models
-        <Torus position={[2, 1, 1]} />
-        <Sphere position={[1, 1, 1]} />
-      </group>
-      // Camera Controls
-      {( isBreakpoint ) ? null : <OrbitControls />}
+    <Canvas
+      shadowMap
+      shadows
+      colorManagement
+      // Camera Config
+      camera={{
+        position: [-4, 2, 5],
+        fov: 60
+      }}>
+      <Suspense fallback={null}>
+        // Lighting
+        <ambientLight intensity={0.3} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} />
+        <pointLight castShadow position={[0, -10, 0]} />
+        <group>
+          // Floor mesh
+          <mesh
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <shadowMaterial attach='material' opacity={0.3} />
+          </mesh>
+          // Models
+          <Torus position={[2, 1, 1]} />
+          <Sphere position={[1, 1, 1]} />
+        </group>
+        // If viewport is tablet or smaller, hide the Orbit Controls
+        {( isBreakpoint ) ? null : <OrbitControls />}
+      </Suspense>
     </Canvas>
   )}
