5e88a3b0d6f43f43b6bb0c253b9d9fb4b17a41d4
Amir Ardalan <amirrardalan@gmail.com>
2021-05-03 14:07:19 -0700

    Improve commenting in Canvas component

--- components/ThreeCanvas.js
+++ components/ThreeCanvas.js
@@ -1,9 +1,9 @@
-import React, { useState, useRef, Suspense } from 'react'
+import { React, useRef, Suspense } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { softShadows, MeshWobbleMaterial, OrbitControls } from '@react-three/drei'
 import { useMediaQuery } from '../utils/mediaQuery'
 
-// Three.js Canvas
+// Three.js Canvas Component
 export default function ThreeCanvas() {
 
   // Set the breakpoint for Orbital Controls rendering (disable on mobile)
@@ -16,7 +16,9 @@ export default function ThreeCanvas() {
   function Torus(props) {
 
     const mesh = useRef()
-    if(mesh.current) useFrame((state, delta) => (
+
+    // Ensure mesh is loaded, then rotate it
+    if (mesh.current) useFrame(() => (
       mesh.current.rotation.x += 0.002,
       mesh.current.rotation.y += 0.002
     ))
@@ -55,17 +57,20 @@ export default function ThreeCanvas() {
   }
 
   return (
+
+    // The Canvas where meshes will be rendered
     <Canvas
       shadowMap
       shadows
       colorManagement
+
       // Camera Config
       camera={{
         position: [-4, 2, 5],
         fov: 60
       }}>
       <Suspense fallback={null}>
-        // Lighting
+
         <ambientLight intensity={0.3} />
         <directionalLight
           castShadow
@@ -81,8 +86,8 @@ export default function ThreeCanvas() {
         />
         <pointLight position={[-10, 0, -20]} />
         <pointLight castShadow position={[0, -10, 0]} />
+
         <group>
-          // Floor mesh
           <mesh
             receiveShadow
             rotation={[-Math.PI / 2, 0, 0]}
@@ -90,11 +95,10 @@ export default function ThreeCanvas() {
             <planeBufferGeometry attach='geometry' args={[100, 100]} />
             <shadowMaterial attach='material' opacity={0.3} />
           </mesh>
-          // Models
           <Torus position={[2, 1, 1]} />
           <Sphere position={[1, 1, 1]} />
         </group>
-        // If viewport is tablet or smaller, hide the Orbit Controls
+
         {( isBreakpoint ) ? null : <OrbitControls />}
       </Suspense>
     </Canvas>
