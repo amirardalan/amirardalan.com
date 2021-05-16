5013706271a6c93039bde8953500ad1a9d722425
Amir Ardalan <amirrardalan@gmail.com>
2021-05-15 17:22:33 -0700

    Set correct font for Toggle component

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -28,7 +28,8 @@ const Toggle = ({ toggleTheme }) => {
         <span css={css`
           color: ${theme.colors.text};
           width: 50px;
-          font-size: 8px;
+          font-family: 'Poppins', Arial, Helvetica, sans-serif;
+          font-size: 9px;
           line-height: .7rem;
           text-transform: uppercase;
           padding: .3rem .5rem 0 0;
