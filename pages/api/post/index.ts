13ef979ac3e03dd599297e1296799fe223efbe1a
Amir Ardalan <amirrardalan@gmail.com>
2021-05-18 18:03:09 -0700

    Remove unneeded interface declaration

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -2,27 +2,19 @@ import { getSession } from 'next-auth/client'
 import prisma from '../../../lib/prisma'
 
 
-declare global {
-  namespace NodeJS {
-    interface Global {
-      teaser: any;
-    }
-  }
-}
-
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content, slug, teaser
 export default async function handle(req: any, res: any) {
-  const { title, slug, teaser, content } = req.body
+  const { title, content, slug, teaser } = req.body
 
   const session = await getSession({ req })
   const result = await prisma.post.create({
     data: {
       title: title,
+      content: content,
       slug: slug,
       teaser: teaser,
-      content: content,
       author: { connect: { email: session?.user?.email } },
     },
   })
