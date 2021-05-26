1aba62f2f3d58cf0945edbd6afd10ad5563abe89
Amir Ardalan <amirrardalan@gmail.com>
2021-05-26 02:45:07 -0700

    Remove unused console logs

--- pages/api/publish/[id].ts
+++ pages/api/publish/[id].ts
@@ -5,9 +5,6 @@ export default async function handle(req, res) {
   const postId = req.query.id
   const isPublished = (req.query.published === 'true') ? false : true
 
-  console.log(postId)
-  console.log(isPublished)
-
   const post = await prisma.post.update({
     where: { id: Number(postId) },
     data: { published: Boolean(isPublished) },
