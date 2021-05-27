ee4f59d38fe4ab3b22e7526b149e7588ae388ad1
Amir Ardalan <amirrardalan@gmail.com>
2021-05-27 02:23:23 -0700

    Add about page content

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,59 @@
 import { useTheme, css } from '@emotion/react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function About() {
 
   const theme : any = useTheme()
+
+  const centerImage = css({
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '6rem 0 4rem 0',
+  })
+
+  const copy = css({
+    marginBottom: '2rem',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    fontFamily: theme.fonts.tertiary,
+    fontSize: 16,
+    p: {
+      padding: '3%',
+    },
+    '@media(max-width: 1024px)': {
+      flexDirection: 'column',
+      p: {
+        padding: '1.5rem',
+      },
+
+    }
+  })
 
   const grid = css({
     display: 'flex',
     justifyContent: 'center',
     padding: '3rem',
     backgroundColor: theme.page.bg,
-    animation: 'slideUp .5s forwards'
+    animation: 'slideUp .5s forwards',
+    ul: {
+      li: {
+        marginBottom: '.5rem',
+        color: theme.colors.grayscale,
+        fontSize: 14,
+        textAlign: 'center',
+      }
+    },
+  })
+
+  const h4 = css({
+    marginBottom: '1.5rem',
+    paddingBottom: '1rem',
+    fontFamily: theme.fonts.secondary,
+    fontSize: 25,
+    textAlign: 'center',
+    borderBottom: '2px solid'+ theme.colors.accentColor
   })
 
 
@@ -18,22 +61,36 @@ export default function About() {
     <div className="about">
       <h2 css={{ 
         fontFamily: theme.fonts.secondary,
-        fontSize: 38,
+        fontSize: 'calc(3.2vw + 3.2vh)',
         fontWeight: 900,
-        marginBottom: 15,
+        textAlign: 'center',
       }}>
-        About
+        Design &amp; Code.
       </h2>
-      <p css={{
+      
+      <h3 css={{
+        marginBottom: '1.5rem',
+        color: theme.colors.grayscale,
         fontFamily: theme.fonts.tertiary,
-        fontSize: 18,
-        marginBottom: '2rem',
-      }}>
-        Hello, I'm Amir Ardalan, a developer and designer from Portland, Oregon. I love React.js and Next.js. 
-        I'll be showcasing my personal projects on this portfolio site as well as writing about code and life.
-        This page is still in development, For now, you can read a bit 
-        about <Link href="/blog/2021-a-dev-odyssey">my journey</Link> as a developer.
-      </p>
+        fontSize: 'calc(1vw + 1vh)',
+        fontWeight: 'normal',
+        fontStyle: 'italic',
+        textAlign: 'center',
+      }}>– from Portland, OR –
+      </h3>
+      <div css={copy}>
+        <p>
+          Hello, I'm Amir Ardalan, a developer and designer with over 10 years experience, based in Portland, Oregon. 
+          I especially love React.js, Next.js., TypeScript, Prisma, and PostgreSQL.
+          Read a bit 
+          about <Link href="/blog/2021-a-dev-odyssey">my journey</Link> as a developer.
+        </p>
+        <p>
+          I'm currently seeking opportunities to join a team that is as passionate about building
+          great products as I am. I have extensive industry experience including; freelance, startup, enterprise,
+          agency, and ecommerce. <Link href="/amir-ardalan-resume.pdf">Download my resume</Link>.
+        </p>
+      </div>
       <main css={{
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 1fr)',
@@ -46,13 +103,115 @@ export default function About() {
           gridTemplateColumns: 'repeat(1, 1fr)',
         }
       }}>
-        <div css={grid}>This</div>
-        <div css={grid}>page</div>
-        <div css={grid}>is</div>
-        <div css={grid}>coming</div>
-        <div css={grid}>soon</div>
-        <div css={grid}>...</div>
+        <div css={grid}>
+          <ul>
+            <h4 css={h4}>Details:</h4>
+            <li>
+              <Image
+                src="/photo.png"
+                alt="Amir Ardalan"
+                aria-label="Amir Ardalan"
+                width={100}
+                height={100}
+              />
+            </li>
+            <li>Designer + Developer</li>
+            <li>Creative and thorough.</li>
+            <li>I ❤️ collaborating.</li>
+            <li>
+              <Link href="/amir-ardalan-resume.pdf">Resume</Link>
+            </li>
+          </ul>
+        </div>
+        <div css={grid}>
+          <ul>
+            <h4 css={h4}>Skills:</h4>
+            <li>Front-end Engineering</li>
+            <li>User Interface Design</li>
+            <li>JavaScript / TypeScript</li>
+            <li>React.js / Next.js</li>
+            <li>CSS in JS / SCSS</li>
+            <li>Prisma + PostgreSQL</li>
+            <li>Testing &amp; documentation</li>
+          </ul>
+        </div>
+        <div css={grid}>
+          <ul>
+            <h4 css={h4}>Experience:</h4>
+            <li>10+ years</li>
+            <li>Columbia Sportswear</li>
+            <li>KEEN Footwear</li>
+            <li>Chrome Industries</li>
+            <li>Hanna Andersson</li>
+            <li>Freelance</li>
+          </ul>
+        </div>
+        <div css={grid}>
+          <ul>
+            <h4 css={h4}>Availability:</h4>
+            <li>✅ Currently Seeking opportunities</li>
+            <li>Remote or Portland, OR</li>
+          </ul>
+        </div>
+        <div css={grid}>
+          <ul>
+            <h4 css={h4}>Social:</h4>
+            <li>
+              <a
+                href="https://github.com/amirardalan"
+                target="_blank"
+                rel="noreferrer noopener"
+                aria-label="GitHub">
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/amirardalan"
+                target="_blank"
+                rel="noreferrer noopener"
+                aria-label="Twitter">
+                Twitter
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://linkedin.com/in/amirardalan"
+                target="_blank"
+                rel="noreferrer noopener"
+                aria-label="LinkedIn">
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div css={grid}>
+          <ul>
+            <h4 css={h4}>Contact</h4>
+            <li>hi@amirardalan.com</li>
+            <li>- or - </li>
+            <li>
+              <a
+                href="https://twitter.com/messages/compose?recipient_id=23831468"
+                className="twitter-dm-button"
+                data-screen-name="@amirardalan"
+                aria-label="DM @amirardalan on Twitter"
+                target="_blank"
+                rel="noreferrer noopener">
+                DM @amirardalan
+              </a>
+          </li>
+          </ul>
+        </div>
       </main>
+      <div css={centerImage}>
+        <Image
+          src={theme.logo}
+          height={100}
+          width={100}
+          alt="Logo"
+        />
+      </div>
     </div>
   )
 }
