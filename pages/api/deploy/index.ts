16b7659d6f495d636ed05b36daa08371dc151d0e
Amir Ardalan <amirrardalan@gmail.com>
2021-05-30 15:42:46 -0700

    Obfuscate deploy url in env var

--- pages/api/deploy/index.ts
+++ pages/api/deploy/index.ts
@@ -1,11 +1,13 @@
 // DEPLOY /api/deploy
 
+const deployUrl = process.env.DEPLOY_HOOK
+
 export default async (req: any, res: any) => {
 
   if (req.query.secret !== `${process.env.NEXT_PUBLIC_DEPLOY_TOKEN}`) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  fetch("https://api.vercel.com/v1/integrations/deploy/prj_NfIWVqIhuOYXs9otAnkaVIe8Z3pl/qdTsNi6eoV")
+  fetch(deployUrl)
   return res.json()
 }
