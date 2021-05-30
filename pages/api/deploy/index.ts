6fe350ddee20ae379bedd8802ee24dab568e1eff
Amir Ardalan <amirrardalan@gmail.com>
2021-05-30 15:13:45 -0700

    Add fetch request to deploy

--- pages/api/deploy/index.ts
+++ pages/api/deploy/index.ts
@@ -1,10 +1,11 @@
 // DEPLOY /api/deploy
+
 export default async (req: any, res: any) => {
 
   if (req.query.secret !== `${process.env.NEXT_PUBLIC_DEPLOY_TOKEN}`) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  process.env.DEPLOY_HOOK
+  fetch(process.env.DEPLOY_HOOK)
   return res.json()
 }
