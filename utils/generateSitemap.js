42ca3eeeff87fb28bdeb4a8a52dfe9f6a0ce1dca
Amir Ardalan <amirrardalan@gmail.com>
2021-05-30 22:13:27 -0700

    Remove dynamically genrated sitemap file

--- utils/generateSitemap.js
+++ utils/generateSitemap.js
@@ -9,6 +9,7 @@ const prettier = require('prettier');
   // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby([
     'pages/**/*{.tsx,.ts}',
+    '.next/**/*{.html,.json}',
     '!pages/_*.tsx',
     '!pages/api',
 		'!pages/404.tsx',
