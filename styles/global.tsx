cb8c6fe8ff8362010238470533771cf5a59e92df
Amir Ardalan <amirrardalan@gmail.com>
2021-05-31 15:27:24 -0700

    Improve theme contrast

--- styles/global.tsx
+++ styles/global.tsx
@@ -344,7 +344,7 @@ export function GlobalStyles () {
 
         .mainRight {
           width: 50%;
-          height: 75vh;
+          height: 72vh;
           position: relative;
           display: flex;
           flex-direction: column;
