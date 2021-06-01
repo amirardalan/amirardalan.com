608171de38fd8f6d4f3c7376290b27eeaaebf9a9
Amir Ardalan <amirrardalan@gmail.com>
2021-05-31 20:58:45 -0700

    Convert CSS to object styles

--- components/BlogLayout.tsx
+++ components/BlogLayout.tsx
@@ -7,279 +7,275 @@ export default function BlogLayout(props) {
   return (
     <>
       <Global
-        styles={css`
-          // Blog Styles
-          .breadcrumbs {
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            color: ${theme.colors.grayscale};
-            font-size: 13px;
-            &::before {
-              padding-right: .5rem;
-              display: flex;
-              content: '📋';
-              color: ${theme.colors.link};
-              font-size: 20px;
-            }
-            a { text-decoration: none;
-              &::after {
-                content: '/';
-                margin: 0 .5rem;
-                color: ${theme.colors.grayscale};
+        styles={{
+          '.breadcrumbs': {
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            color: theme.colors.grayscale,
+            fontSize: 13,
+            '&::before': {
+              paddingRight: '.5rem',
+              display: 'flex',
+              content: '"📋"',
+              color: theme.colors.link,
+              fontSize: 20,
+            },
+            a: {
+              textDecoration: 'none',
+              '&::after': {
+                content: '"/"',
+                margin: '0 .5rem',
+                color: theme.colors.grayscale,
+              }
+            },
+            '@media (max-width: 480px)': {
+              span: {
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis',
               }
             }
-            @media (max-width: 480px) {
-              span {
-                overflow: hidden;
-                white-space: nowrap;
-                text-overflow: ellipsis;
-              }
-            }
-          }
-
-          .blog {
-            max-width: 768px;
-            margin: 0 auto;
+          },
+          '.blog': {
+            maxWidth: 768,
+            margin: '0 auto',
             
-            @media (max-width: 480px) {
-              margin: 0 1%;
+            '@media (max-width: 480px)': {
+              margin: '0 1%',
+            },
+            'h2, h3, h4': {
+              fontFamily: theme.fonts.secondary,
+            },
+            h2: {
+              margin: 0,
+              display: 'inline-block',
+              lineHeight: '2.5rem',
+              fontSize: 38,
+              fontWeight: 900,
+              cursor: 'pointer',
+              textDecoration: 'underline',
+              '&:hover': { textDecoration: 'none' }
+            },
+            p: {
+              fontFamily: theme.fonts.tertiary,
+              fontSize: 18,
+            },
+            main: {
+              display: 'flex',
+              flexDirection: 'column',
             }
-            h2, h3, h4 {
-              font-family: ${theme.fonts.secondary};
-            }
-            h2 {
-              margin: 0;
-              display: inline-block;
-              line-height: 2.5rem;
-              font-size: 38px;
-              font-weight: 900;
-              cursor: pointer;
-              text-decoration: underline;
-              &:hover { text-decoration: none; }
-            }
-            p {
-              font-family: ${theme.fonts.tertiary};
-              font-size: 18px;
-            }
-            main {
-              display: flex;
-              flex-direction: column;
-            }
-          }
+          },
+          '.post': {
+            marginTop: '3rem',
+          },
+          '.postDetails': {
+            margin: '.6rem 0 .2rem',
+            color: theme.colors.grayscale,
+            fontSize: 13,
 
-          .post {
-            margin-top: 3rem;
-          }
-
-          .postDetails {
-            margin: .6rem 0 .2rem;
-            color: ${theme.colors.grayscale};
-            font-size: 13px;
-
-            @media (max-width: 480px) {
-              font-size: 12px;
+            '@media (max-width: 480px)': {
+              fontSize: 12,
             }
-          }
+          },
 
-          .controlsPost {
-            margin: 2rem 0;
-          }
+          '.controlsPost': {
+            margin: '2rem 0',
+          },
 
-          .formSubmit {
-            margin-top: 1rem;
-          }
-          
-          .controlsConfirm {
-            margin: 1rem 0 0 0;
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            font-size: 12px;
-            font-weight: lighter;
-            text-transform: uppercase;
-            .confirmLink {
-              margin-right: .5rem;
-              color: ${theme.colors.text};
-              font-size: 12px;
-              font-weight: bold;
-              text-transform: none;
-              cursor: pointer;
-              &.delete {
-                color: #ff6262;
-              }
-              &.close {
-                margin-left: .5rem;
-              }
-              &.delete:hover,
-              &.close:hover {
-                text-decoration: underline;
+          '.formSubmit': {
+            marginTop: '1rem',
+          },
+          '.controlsConfirm': {
+            margin: '1rem 0 0 0',
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            fontSize: 12,
+            fontWeight: 'lighter',
+            textTransform: 'uppercase',
+            '.confirmLink': {
+              marginRight: '.5rem',
+              color: theme.colors.text,
+              fontSize: 12,
+              fontWeight: 'bold',
+              textTransform: 'none',
+              cursor: 'pointer',
+              '&.delete': {
+                color: '#ec4949'
+              },
+              '&.close': {
+                marginLeft: '.5rem',
+              },
+              '&.delete:hover, &.close:hover': {
+                textDecoration: 'underline',
               }
             }
-          }
-
-          .postDraft {
-            margin: 1rem 0 .5rem;
-            padding: 1.8rem;
-            display: flex;
-            justify-content: space-between;
-            border: 1px solid ${theme.colors.accent};
-            &::after {
-              content: 'Draft';
-              align-self: right;
-              color: ${theme.colors.grayscale};
-              font-size: 12px;
-              font-style: italic;
-            }
-            @media (max-width: 480px) {
-              padding: 1rem;
-              &::after { content: '' }
-              h2 {
-                line-height: 2.1rem;
-                font-size: 20px;
+          },
+          '.postDraft': {
+            margin: '1rem 0 .5rem',
+            padding: '1.8rem',
+            display: 'flex',
+            justifyContent: 'space-between',
+            border: '1px solid' + theme.colors.accent,
+            '&::after': {
+              content: '"Draft"',
+              alignSelf: 'right',
+              color: theme.colors.grayscale,
+              fontSize: 12,
+              fontStyle: 'italic',
+            },
+            '@media (max-width: 480px)': {
+              padding: '1rem',
+              '&::after': { content: '" "' },
+              h2: {
+                lineHeight: '2.1rem',
+                fontSize: 20,
               }
+            },
+            '.postTeaser': {
+              margin: '0 .5rem',
             }
-            .postTeaser {
-              margin: 0 .5rem;
-            }
-          }
+          },
+          '.postTeaser': {
+            h2: { fontSize: 30 }
+          },
 
-          .postTeaser {
-            h2 { font-size: 30px; }
-          }
-
-          .postFull {
-            h2 {
-              text-decoration: none;
-              cursor: default;
-            }
-            p { margin-top: 2rem; }
-            p, ul, li, a, blockquote {
-              font-family: ${theme.fonts.tertiary}
-            }
-            h3 {
-              margin: 3.5rem 0 0;
-              padding: 0;
-              font-size: 28px;
-              font-weight: bold;
-            }
-            p, ul, li, a {
-              font-size: 18px;
-              line-height: 1.8rem;
-            }
-            ul, li, a { margin-bottom: 1rem; }
-            p { margin-bottom: 2rem; }
-            a { text-decoration: underline;
-              &:hover { text-decoration: none; }
-            }
-            blockquote {
-              margin-left: -1.75rem;
-              padding-left: 1.5rem;
-              border-left: 5px solid ${theme.colors.accent};
-              color: ${theme.colors.grayscale};
-              font-style: italic;
-              font-weight: 400;
-              & blockquote {
-                margin-left: 0;
-                padding-left: 1rem;
-                border-left: 5px solid ${theme.colors.accent};
-              }
-              @media (max-width: 890px) {
-                margin-left: -1.1rem;
-                padding-left: .8rem;
-              }
-              p { font-size: 24px; }
-            }
-            ul li {
-              list-style: outside;
-              margin-left: 2rem;
-              padding-left: .5rem;
+          '.postFull': {
+            h2: {
+              textDecoration: 'none',
+              cursor: 'default',
+            },
+            p: {
+              marginTop: '2rem',
+              marginBottom: '2rem',
+            },
+            'p, ul, li, a, blockquote': {
+              fontFamily: theme.fonts.tertiary,
+            },
+            h3: {
+              margin: '3.5rem 0 0',
+              padding: 0,
+              fontSize: 28,
+              fontWeight: 'bold',
+            },
+            'p, ul, li, a': {
+              fontSize: 18,
+              lineHeight: '1.8rem',
+            },
+            'ul, li, a': { 
+              marginBottom: '1rem'
+            },
+            a: {
+              textDecoration: 'underline',
+              '&:hover': {
+                textDecoration: 'none'
+              },
+            },
+            blockquote: {
+              marginLeft: '-1.75rem',
+              paddingLeft: '1.5rem',
+              borderLeft: '5px solid' + theme.colors.accent,
+              color: theme.colors.grayscale,
+              fontStyle: 'italic',
+              fontWeight: 'normal',
+              '& blockquote': {
+                marginLeft: 0,
+                paddingLeft: '1rem',
+                borderLeft: '5px solid' + theme.colors.accent,
+              },
+              '@media (max-width: 890px)': {
+                marginLeft: '-1.1rem',
+                paddingLeft: '.8rem',
+              },
+              p: { fontSize: 24 },
+            },
+            'ul li': {
+              listStyle: 'outside',
+              marginLeft: '2rem',
+              paddingLeft: '.5rem',
               
-              @media (max-width: 480px) {
-                margin-left: 1.5rem;
+              '@media (max-width: 480px)': {
+                marginLeft: '1.5rem',
+              }
+            },
+            ol: {
+              li: {
+                listStyleType: 'decimal',
+                marginLeft: '2rem',
+                paddingLeft: '.5rem',
               }
             }
-            ol {
-              li {
-                list-style-type: decimal;
-                margin-left: 2rem;
-                padding-left: .5rem;
-              }
-            }
-          }
-
+          },
           // Next Prev Controls
-          .nextPrevControls {
-            display: flex;
-            justify-content: space-between;
-            a {
-              text-decoration: underline;
-              &:hover {
-                text-decoration: none;
+          '.nextPrevControls': {
+            display: 'flex',
+            justifyContent: 'space-between',
+            a: {
+              textDecoration: 'underline',
+              '&:hover': {
+                textDecoration: 'none',
               }
+            },
+            '@media(max-width: 768px)': {
+              flexDirection: 'column',
             }
-            @media(max-width: 768px) {
-              flexDirection: column;
-            }
-          }
-          .prevLink {
-            display: flex;
-            text-align: left;
-            margin-right: 1rem;
-          }
-          .nextLink {
-            display: flex;
-            text-align: right;
-          }
-          .prevLink,
-          .nextLink {
-            font-family: ${theme.fonts.tertiary};
-            font-size: 18px;
-          }
+          },
+          '.prevLink': {
+            display: 'flex',
+            textAlign: 'left',
+            marginRight: '1rem',
+          },
+          '.nextLink': {
+            display: 'flex',
+            textAlign: 'right',
+          },
+          '.prevLink, .nextLink': {
+            fontFamily: theme.fonts.tertiary,
+            fontSize: 18,
+          },
 
           // Flex Utils
-          .center {
-            display: flex;
-            justify-content: center;
-          }
+          '.center': {
+            display: 'flex',
+            justifyContent: 'center',
+          },
 
           // Markdown Code
-          pre,
-          code {
-            background-color: #2a2734;
-            border-radius: 5px;
-            color: ${theme.colors.background};
-          }
-          code {
-            word-wrap: break-word;
-          }
-          pre {
-            border: 1px solid ${theme.colors.accent};
-            padding: 1rem;
-            line-height: 2rem;
-            code {
-              word-wrap: normal;
+          'pre, code': {
+            backgroundColor: '#2a2734',
+            borderRadius: 5,
+            color: theme.colors.background,
+          },
+          code: {
+            wordWrap: 'break-word',
+          },
+          pre: {
+            padding: '1rem',
+            lineHeight: '2rem',
+            code: {
+              wordWrap: 'normal',
             }
-          }
-          code, code p {
-            padding: 0.2rem;
-            background-color: ${theme.colors.accent};
-            color: ${theme.colors.text};
-            font-size: 15px;
-          }
-          .codeStyle {
-            code {
-              padding: 0;
+          },
+          'code, code p': {
+            padding: '0.2rem',
+            backgroundColor: theme.colors.accent,
+            color: theme.colors.text,
+            fontSize: 15,
+          },
+          '.codeStyle': {
+            code: {
+              padding: 0,
             }
-          }
-          .language-bash {
-            .codeStyle & {
-              pre & {
-                span.linenumber { display: none !important; }
+          },
+          '.languageBash': {
+            '.codeStyle &': {
+              'pre &': {
+                'span.linenumber': { display: 'none !important', }
               }
             }
           }
-        `}
+        }}
       />
       <div className="layout">
         {props.children}
