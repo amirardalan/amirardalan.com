ea896f9236acef692d84eefdd79fd6962891fd46
Amir Ardalan <amirrardalan@gmail.com>
2021-06-01 17:43:41 -0700

    Revert dynamic loader

--- components/CanvasLoader.tsx
+++ components/CanvasLoader.tsx
@@ -2,13 +2,7 @@ import React from 'react'
 import { useTheme } from '@emotion/react'
 import { Canvas } from '@react-three/fiber'
 import CanvasText from './CanvasText'
-import LoadingTriangle from '../components/LoadingTriangle'
-import dynamic from 'next/dynamic'
-
-const CanvasScene = dynamic(() => import('../components/CanvasScene'), {
-  loading: () => <LoadingTriangle />,
-  ssr: false
-})
+import CanvasScene from './CanvasScene'
 
 const CanvasLoader =  React.memo(() => {
 
