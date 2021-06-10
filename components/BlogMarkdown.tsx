a91532fafc0fd8709997fe0463956099d786e886
Amir Ardalan <amirrardalan@gmail.com>
2021-06-10 10:24:19 -0700

    Output metastring WIP

--- components/BlogMarkdown.tsx
+++ components/BlogMarkdown.tsx
@@ -2,6 +2,7 @@ import { useTheme } from '@emotion/react'
 import { materialLight, materialOceanic } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import ReactMarkdown from 'react-markdown'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import rangeParser from 'parse-numeric-range'
 import gfm from 'remark-gfm'
 import rehypeSlug from 'rehype-slug'
 import link from 'rehype-autolink-headings'
@@ -17,7 +18,24 @@ export default function BlogMarkdown({ props }) {
   // Syntax Highlighter Object for Markdown
   const BlogSyntaxHighlight: object = {
     code({node, inline, className,...props}) {
+
       const match = /language-(\w+)/.exec(className || '')
+      const metadata = node.data.meta
+      const metastring = node.data.meta.replace(/[{}]/g, "")
+
+      const calculateLinesToHighlight : any = (metastring : any) => {
+        const RE = /{([\d,-]+)}/
+        console.log(RE.test(metastring))
+        if (RE.test(metastring)) {
+          const strlineNumbers : any = RE.exec(metastring)[1]
+          const lineNumbers : any = rangeParser(strlineNumbers)
+          return (index : number) => (lineNumbers.includes(index + 1))
+        } else {
+          return () => false
+        }
+      }
+      // const shouldHighlightLine: any = calculateLinesToHighlight(metastring)
+      // if (shouldHighlightLine(index)) { lineProps.className = `${lineProps.className} highlight-line` }
       return match ? (
         <SyntaxHighlighter
           style={setSyntaxTheme}
@@ -25,6 +43,8 @@ export default function BlogMarkdown({ props }) {
           PreTag="div"
           className="codeStyle"
           showLineNumbers={false}
+          wrapLines={true}
+          lineProps={calculateLinesToHighlight}
           {...props}
         />
       ) : (
@@ -35,12 +55,19 @@ export default function BlogMarkdown({ props }) {
 
   return (
     <>
+      <ReactMarkdown
+        children={props.post.content}
+        components={BlogSyntaxHighlight}
+        remarkPlugins={[ [gfm] ]}
+        rehypePlugins={[ [rehypeSlug], [link] ]}
+      />
       <Global styles={{
         '.codeStyle, pre, code, code span': {
           fontFamily: 'var(--font-primary)',
           fontStyle: 'normal !important'
         },
         '.codeStyle': {
+          padding: '1rem !important',
           overflow: 'scroll',
           borderRadius: 5,
           backgroundColor: 'var(--syntax-highlight-bg) !important',
@@ -67,13 +94,6 @@ export default function BlogMarkdown({ props }) {
           display: 'none !important'
         },
       }} />
-
-      <ReactMarkdown
-        children={props.post.content}
-        components={BlogSyntaxHighlight}
-        remarkPlugins={[ [gfm] ]}
-        rehypePlugins={[ [rehypeSlug], [link] ]}
-      />
     </>
   )
 }
