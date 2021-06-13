76bae4d29173d5ecb853072a483c64a99888559a
Amir Ardalan <amirrardalan@gmail.com>
2021-06-13 00:53:54 -0700

    Complete Spotify banner styling

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -8,43 +8,76 @@ export default function NowPlaying() {
 
   const styleNowPlayingContainer = css({
     marginBottom: '2rem',
-    // padding: '2rem',
     display: 'flex',
-    flexDirection: 'column',
     background: 'var(--color-accent)',
   })
 
   const styleNowPlaying = css({
-    justifyContent: 'space-around',
     position: 'relative',
     height: 200,
     width: '100%',
     overflow: 'hidden',
+    '@media(max-width: 480px)': {
+      height: 300
+    },
   })
   const styleNowPlayingInner = css({
-    display: 'flex',
-    flexDirection: 'column',
+    padding: '2rem',
     height: 200,
     width: '100%',
     alignItems: 'flex-start',
     position: 'absolute',
     top: 0,
     left: 0,
+    color: '#eee',
     background: 'linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.5518867924528301) 100%)',
+    'a, p': {
+      margin: 0,
+      padding: 0,
+      color: '#eee',
+      textDecoration: 'none',
+    },
+    '@media(max-width: 480px)': {
+      height: 300
+    },
+  })
+  const styleNowPlayingTrack = css({
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    a: {
+      fontSize: 'calc(1.2vw + 1.2vh)',
+      fontFamily: 'var(--font-secondary)',
+    },
+    p: {
+      fontSize: 'calc(1vw + 1vh)',
+    },
+    '.trackText': {
+      marginBottom: '1rem',
+    },
+    '@media(max-width: 480px)': {
+      marginTop: '1rem',
+      lineHeight: '.8rem',
+      flexDirection: 'column',
+      alignItems: 'center',
+      textAlign: 'center',
+    },
   })
   const styleNowPlayingBackground = css({
     height: 200,
-    width: '120%',
+    width: '100%',
     backgroundImage: `url(${data?.albumImageUrl})`,
     backgroundSize: 'cover',
-    backgroundPosition: '-50%',
+    backgroundPositionX: '50%',
+    backgroundPositionY: '50%',
     backgroundRepeat: 'no-repeat',
+    '@media(max-width: 480px)': {
+      height: 300
+    },
   })
-  const styleNowPlayingTrack = css({
-    fontFamily: 'var(--font-secondary)',
-    fontSize: 'calc(1.5vw + 1.5vh)',
-    lineHeight: '2.2rem',
-  })
+
 
   return (
     <div css={styleNowPlayingContainer}>
@@ -53,24 +86,28 @@ export default function NowPlaying() {
         <div css={styleNowPlayingInner}>
         {data?.songUrl ? (
           <>
-            <Image
-              src={data?.albumImageUrl}
-              height='150'
-              width='150'
-              alt={data.title}
-            />
             <div css={styleNowPlayingTrack}>
-              <a
-                href={data.songUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {data.title}
-              </a>
+              <span className="trackText">
+                <a
+                  href={data.songUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {data.title} {' - '}
+                </a>
+                <p>
+                  {data?.artist ?? 'Spotify'}
+                </p>
+              </span>
+              <span>
+                <Image
+                  src={data?.albumImageUrl}
+                  height='135'
+                  width='135'
+                  alt={data.title}
+                />
+              </span>
             </div>
-            <p>
-              {data?.artist ?? 'Spotify'}
-            </p>
           </>
         ) : (
           <p>
