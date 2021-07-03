10b1c4ea3c7e2bf34595be1d7cf674a190078b22
Amir Ardalan <amirrardalan@gmail.com>
2021-07-03 04:17:38 -0700

    Remove unused GTM script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -61,10 +61,6 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
 
           <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
-          />
-          <script
             dangerouslySetInnerHTML={{__html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
