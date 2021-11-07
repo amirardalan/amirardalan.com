586472c00b9ca709ec7333d19bb896597870ab2d
Amir A <amirrardalan@gmail.com>
2021-11-06 21:44:19 -0700

    Update sitemap

--- scripts/generateSitemap.mjs
+++ scripts/generateSitemap.mjs
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const globby = require('globby');
-const prettier = require('prettier');
+import { writeFileSync } from 'fs'
+import { globby } from 'globby'
+import prettier from 'prettier'
 
-(async () => {
-  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
 
+async function generate() {
+  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
   const pages = await globby([
     '.next/server/pages/blog/**/*.json',
     'pages/**/*{.tsx,.ts}',
@@ -14,33 +14,36 @@ const prettier = require('prettier');
     '!pages/_*.tsx',
     '!pages/404.tsx',
   ]);
+
   const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages
-      .map((page) => {
-        const path = page
-          .replace('.next/server/', '')
-          .replace('pages', '')
-          .replace('/index', '')
-          .replace('.tsx', '')
-          .replace('.json', '')
-        const route = path === '/index' ? '' : path;
-
-        return `
-          <url>
-              <loc>${`${process.env.NEXT_PUBLIC_SITE_URL}${route}`}</loc>
-          </url>
-        `;
-      })
-      .join('')}
+        ${pages
+          .map((page) => {
+            const path = page
+              .replace('.next/server/', '')
+              .replace('pages', '')
+              .replace('/index', '')
+              .replace('.tsx', '')
+              .replace('.json', '')
+            const route = path === '/index' ? '' : path;
+            return `
+              <url>
+                <loc>${`${process.env.NEXT_PUBLIC_SITE_URL}${route}`}</loc>
+              </url>
+            `
+          })
+          .join('')}
     </urlset>
-  `
+    `
 
   const formatted = prettier.format(sitemap, {
     ...prettierConfig,
     parser: 'html'
   });
 
-  fs.writeFileSync('public/sitemap.xml', formatted);
-})();
+  // eslint-disable-next-line no-sync
+  writeFileSync('public/sitemap.xml', formatted)
+}
+
+generate();
