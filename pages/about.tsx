515e12b5ee7c1ab7cb626d63f1595fb35d392634
Amir A <amirrardalan@gmail.com>
2022-02-17 18:22:18 -0800

    Serialize drafts data

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image'
 import Head from 'next/head'
 import Avatar from '@/components/Avatar'
 import Timeline from '@/components/Timeline'
-import Uses from '@/components/Uses'
 
 import { GenerateCtaButtons } from '@/components/CtaButtons'
 import { about } from '@/data/content'
