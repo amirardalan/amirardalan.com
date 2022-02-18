bfa3e4a0a69bd200ed5a23257cce8cc076f2ce57
Amir Ardalan <amirrardalan@gmail.com>
2022-02-18 06:59:55 -0800

    Remove unused querystring include

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,5 +1,3 @@
-import querystring from 'querystring'
-
 const client_id = process.env.SPOTIFY_CLIENT_ID
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN
