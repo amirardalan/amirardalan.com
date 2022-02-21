3e36b9003ecc86f1d4d680e9e6e98155bfadab17
Amir A <amirrardalan@gmail.com>
2022-02-21 00:16:11 -0800

    Update BlogError

--- components/BlogError.tsx
+++ components/BlogError.tsx
@@ -1,29 +1,6 @@
-import { css } from '@emotion/react'
-import Link from 'next/link'
-import { error } from '@/data/content'
+import LoadingTriangle from '@/components/LoadingTriangle'
 
 export default function BlogError() {
 
-  const styleBlogError = css({
-    a: { fontFamily: 'var(--font-secondary)' }
-  })
-
-  return (
-    <div css={styleBlogError}>
-      <div>
-        <div className="blogErrorHeading">
-          <h1 className="pageHeading">
-            {error.blogPostError.title}
-          </h1>
-        </div>
-        <p>{error.blogPostError.text}</p>
-        <Link
-          href={error.blogPostError.link.title}
-          aria-label={error.blogPostError.link.title}
-        >
-          {error.blogPostError.link.title}
-        </Link>
-      </div>
-    </div>
-  )
+  return <LoadingTriangle />
 }
