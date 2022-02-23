ac17fcf6e5d3fa35c5060a0f64f66d9bf135e3f0
Amir Ardalan <amirrardalan@gmail.com>
2022-02-23 01:19:59 -0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,88 +1,86 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
+export default function Document() {
 
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="preload"
-            href="/fonts/fira-code-latin-400.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/poppins-latin-700.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/poppins-latin-900.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/lora-latin-500.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/lora-latin-500-italic.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link rel="icon" href="/favicon.png" />
-          <link rel="apple-touch-icon" href="/touch-192.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/fira-code-latin-400.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/poppins-latin-700.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/poppins-latin-900.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/lora-latin-500.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/lora-latin-500-italic.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/touch-192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
 
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{__html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
-                cookie_flags: 'SameSite=None;Secure',
-                page_path: window.location.pathname,
-              });
-            `,
-            }}
-          />
-        </Head>
-        <body>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              function getUserPreference() {
-                if(window.localStorage.getItem('theme')) {
-                  return window.localStorage.getItem('theme')
-                }
-                return window.matchMedia('(prefers-color-scheme: dark)').matches
-                ? 'dark'
-                : 'light'
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+              cookie_flags: 'SameSite=None;Secure',
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
+      <body>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            function getUserPreference() {
+              if(window.localStorage.getItem('theme')) {
+                return window.localStorage.getItem('theme')
               }
-              document.body.dataset.theme = getUserPreference();
-            `,
-            }}
-          />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+              return window.matchMedia('(prefers-color-scheme: dark)').matches
+              ? 'dark'
+              : 'light'
+            }
+            document.body.dataset.theme = getUserPreference();
+          `,
+          }}
+        />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
