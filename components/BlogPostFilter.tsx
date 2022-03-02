06cee8091630ebfc5f99f85060b77c41c90991f0
Amir Ardalan <amirrardalan@gmail.com>
2022-03-02 15:17:29 -0800

    Sort categories

--- components/BlogPostFilter.tsx
+++ components/BlogPostFilter.tsx
@@ -111,7 +111,7 @@ const BlogPostFilter = ({ feed }) => {
               All
             </button>
           </li>
-          {activeCategories.map((category, index) => (
+          {activeCategories.sort().reverse().map((category, index) => (
             <li key={index}>
               <button
                 onClick={() => handleCategoryLink(category)}
