9f8972fb2ab077052ea3f7dd9d771361928c3130
Amir Ardalan <amirrardalan@gmail.com>
2022-03-06 16:48:35 -0800

    Fix navigation layout shift

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,7 +36,7 @@ const Header = ({ toggleTheme }) => {
     justifyContent: 'space-between',
     a:  { textDecoration: 'none' },
     '.headerRight': {
-      minWidth: '391.2px',
+      minWidth: '395.2px',
       display: 'flex',
       flexDirection: 'row',
       alignItems: 'center',
