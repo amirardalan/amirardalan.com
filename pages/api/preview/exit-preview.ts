fa44aa843b4f2f730c30e32124a5fe85763aba01
Amir Ardalan <amirrardalan@gmail.com>
2022-03-07 21:23:14 -0800

    Fix deploy and preview api request handlers

--- pages/api/preview/exit-preview.ts
+++ pages/api/preview/exit-preview.ts
@@ -2,12 +2,12 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 
 // EXIT PREVIEW MODE /api/preview/exit-preview
-// eslint-disable-next-line import/no-anonymous-default-export
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
 
   if (req.query.secret !== `${process.env.NEXT_PUBLIC_PREVIEW_TOKEN}`) {
     return res.status(401).json({ message: 'Invalid token' })
   }
+  
   res.clearPreviewData()
   res.writeHead(307, { Location: "/" })
   res.end()
