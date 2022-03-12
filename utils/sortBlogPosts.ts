ac37003e15205851f50aab29e2b5b13e237d49b0
Amir A <amirrardalan@gmail.com>
2022-03-12 14:34:25 -0800

    Improve Types

--- utils/sortBlogPosts.ts
+++ utils/sortBlogPosts.ts
@@ -1,5 +1,7 @@
 // Sort Blog posts by ID
-export default function sortBlogPosts( a:any, b:any ) {
+type SortID = { id: number }
+
+export default function sortBlogPosts(a: SortID, b: SortID) {
   if ( a.id < b.id ) return -1
   if ( a.id > b.id ) return 1
   return 0
