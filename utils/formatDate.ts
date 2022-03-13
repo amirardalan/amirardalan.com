6c3b25ed2f91f3ed2786c5e1106cc77bd70def85
Amir Ardalan <amirrardalan@gmail.com>
2022-03-12 21:30:26 -0800

    Fix date formatting util

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,14 +1,13 @@
 // Format Date - (eg: January 1, 2021)
-const FormatDate = (date: String) => {
-  const dateObj = new Date(date.slice(0, 10))
-  const formatDate = [
-    dateObj.toLocaleDateString("en-US", { month: 'long' })+' '+
-    dateObj.toLocaleDateString("en-US", { day: 'numeric' })+', '+
-    dateObj.toLocaleDateString("en-US", { year: 'numeric' })
-  ]
+const FormatDate = (date: Date) => {
+  const formatDate = new Date(date).toLocaleDateString('en-US', {
+    timeZone: 'UTC',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  })
   
-  const postDate = formatDate.toString()
-  return postDate as String
+  return formatDate
 }
 
 export default FormatDate
