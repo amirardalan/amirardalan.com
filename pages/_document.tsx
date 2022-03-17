f45efc4af53b20429d25011e4a1f129ad5a03b90
Amir A <amirrardalan@gmail.com>
2022-03-17 06:46:49 -0700

    Migrate to GA4 analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -51,13 +51,10 @@ export default function Document() {
         />
         <script
           dangerouslySetInnerHTML={{__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
-              cookie_flags: 'SameSite=None;Secure',
-              page_path: window.location.pathname,
-            });
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
           `,
           }}
         />
