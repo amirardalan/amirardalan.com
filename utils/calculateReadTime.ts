8546a4b760339f88972b1d0d25a9b68e16f31d80
Amir A <amirrardalan@gmail.com>
2022-03-19 19:37:46 -0700

    Remove unnecessary variable

--- utils/calculateReadTime.ts
+++ utils/calculateReadTime.ts
@@ -1,8 +1,7 @@
 // Calculate estimated read time of "content"
 const ReadTime = (content: string) => {
-  const text: string = content
   const wpm = 200
-  const words = text.trim().split(/\s+/).length
+  const words = content.trim().split(/\s+/).length
   const time = Math.ceil(words / wpm)
   const readTime = time + ' ' + 'min read'
   return readTime as string
