5a477f776220c21d2bdb87ab513a06e9f8f22396
Amir Ardalan <amirrardalan@gmail.com>
2022-03-23 08:00:33 -0700

    Update cms textarea caret color

--- styles/global.tsx
+++ styles/global.tsx
@@ -188,8 +188,9 @@ export function GlobalStyles () {
           margin: '4rem 0 1.5em 0',
           padding: 0,
         },
-        'input, select': {
+        'input, textarea, select': {
           verticalAlign: 'middle',
+          caretColor: 'var(--color-text)'
         },
         // Base
         'html, body': {
