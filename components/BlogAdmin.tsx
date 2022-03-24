371b2aed97f10a00965bad12a528d3c70c421a73
Amir A <amirrardalan@gmail.com>
2022-03-24 03:27:12 -0700

    Clean up blog admin component

--- components/BlogAdmin.tsx
+++ components/BlogAdmin.tsx
@@ -8,14 +8,16 @@ import LoadingSpinner from './LoadingSpinner'
 
 const BlogAdmin = React.memo(function BlogAdmin() {
 
-  const router = useRouter()
-  const isActive: (pathname: string) => boolean = (pathname) => router.pathname === pathname
   const { data: session } = useSession()
+  const router = useRouter()
+  const URL = process.env.NEXT_PUBLIC_SITE_URL
+
+  // Session & Route Conditionals
   const isLoggedIn = session && session.user.email == process.env.NEXT_PUBLIC_USER_EMAIL
+  const isAdminPage = ['/blog/create','/blog/edit/[id]','/blog/drafts'].includes(router.pathname)
+  const isActive: (pathname: string) => boolean = (pathname) => router.pathname === pathname
 
-  let adminPanelLeft = null
-  let adminPanelRight = null
-
+  // Deploy Hook
   const [isDeploying, setIsDeploying] = useState(false)
   const showDeployLoader: Function = () => {
     setIsDeploying(true)
@@ -46,13 +48,9 @@ const BlogAdmin = React.memo(function BlogAdmin() {
     display: 'flex',
     padding: '.5rem 4rem',
     backgroundColor: 'var(--color-accent)',
-    borderBottom: '1px dotted var(--color-neutral)',
-    flexDirection: 'row',
+    borderBottom: '1px solid var(--color-accent-neutral)',
     justifyContent: 'space-between',
     animation: 'adminPanelSlideDown .2s',
-    '@media (max-width: 1024px)': {
-      padding: '.5rem 2.5rem'
-    },
     '@media (max-width: 600px)': {
       flexDirection: 'column',
       padding: '.5rem 1rem'
@@ -77,16 +75,13 @@ const BlogAdmin = React.memo(function BlogAdmin() {
       margin: '0 .5rem .5rem 0',
     },
     span: {
-      display: 'flex',
       fontSize: 11,
       alignSelf: 'center',
-      color: 'var(--color-text)',
-      fontFamily: 'var(--font-primary)',
+      color: 'var(--color-neutral)',
       a: {
         marginLeft: '.5rem',
         textTransform: 'uppercase',
         textDecoration: 'none',
-        cursor: 'pointer',
         '&:hover': {
           textDecoration: 'underline'
         }
@@ -96,68 +91,52 @@ const BlogAdmin = React.memo(function BlogAdmin() {
   const styleAdminPanelRight = css({
     display: 'flex',
     justifyContent: 'right',
-    flexDirection: 'row',
-    alignSelf: 'flex-end',
     '.deploymentStatus': {
       display: 'flex',
-      flexAlign: 'row',
     },
     'button, a': {
       marginRight: '.25rem',
     },
   })
 
-  const URL = process.env.NEXT_PUBLIC_SITE_URL
-  const isAdminPage = ['/blog/create','/blog/edit/[id]','/blog/drafts'].includes(router.pathname)
-
-  if (isLoggedIn) {
-    adminPanelLeft = (
-      <div css={styleAdminPanelLeft}>
-        <span>
-          Welcome, {session.user.name.split(" ")[0]}! •
-          <a
-            onClick={() => signOut({
-              callbackUrl: isAdminPage ? `${URL}/blog` : `${URL+router.asPath}`
-            })}
-            aria-label="Sign Out"
-            tabIndex={0}
-          >
-            Sign Out
-          </a>
-        </span>
-      </div>
-    )
-    adminPanelRight = (
-      <div css={styleAdminPanelRight}>
-        <div className="deploymentStatus">
-          { isDeploying ? <LoadingSpinner /> : null }
-          <button
-            onClick={ !isDeploying ? deployNewBuild : null }
-            className={ (isDeploying) ? 'buttonCompact deploy disabled' : 'buttonCompact deploy' }
-            aria-label="Deploy"
-          >
-            Deploy
-          </button>
-        </div>
-        <Link href="/blog/create" passHref>
-          <button className="buttonCompact createBtn" aria-label="New Post">
-            Create
-          </button>
-        </Link>
-        <Link href="/blog/drafts" passHref aria-label="Drafts">
-          <button className="buttonCompact draftsBtn" data-active={isActive('/drafts')}>
-            Drafts
-          </button>
-        </Link>
-      </div>
-    )
-  }
-
   const AdminPanel = () => {
     return (
       <>
-        {adminPanelLeft}
-        {adminPanelRight}
+        <div css={styleAdminPanelLeft}>
+          <span>
+            Welcome, {session.user.name.split(" ")[0]}! •
+            <a
+              onClick={() => signOut({ callbackUrl: isAdminPage ? `${URL}/blog` : `${URL+router.asPath}`})}
+              aria-label="Sign Out"
+              tabIndex={0}
+            >
+              Sign Out
+            </a>
+          </span>
+        </div>
+
+        <div css={styleAdminPanelRight}>
+          <div className="deploymentStatus">
+            { isDeploying ? <LoadingSpinner /> : null }
+            <button
+              onClick={ !isDeploying ? deployNewBuild : null }
+              className={ (isDeploying) ? 'buttonCompact deploy disabled' : 'buttonCompact deploy' }
+              aria-label="Deploy"
+            >
+              Deploy
+            </button>
+          </div>
+          <Link href="/blog/create" passHref>
+            <button className="buttonCompact createBtn" aria-label="New Post">
+              Create
+            </button>
+          </Link>
+          <Link href="/blog/drafts" passHref aria-label="Drafts">
+            <button className="buttonCompact draftsBtn" data-active={isActive('/drafts')}>
+              Drafts
+            </button>
+          </Link>
+        </div>
       </>
     )
   }
@@ -301,7 +280,7 @@ const BlogAdmin = React.memo(function BlogAdmin() {
       }}/>
       <div css={styleAnimationWrapper}>
         <nav css={styleAdminPanel}>
-          <AdminPanel/>
+          {isLoggedIn ? <AdminPanel/> : null}
         </nav>
       </div>
     </>
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,22 +1,11 @@
-import BlogStyles from '@/components/BlogStyles'
-import Container from '@/components/Container'
-import BlogPostFilter from '@/components/BlogPostFilter'
-
-import { blog } from '@/data/content'
 import { GetStaticProps } from 'next'
 import prisma from '@/lib/prisma'
 
+import Container from '@/components/Container'
+import BlogStyles from '@/components/BlogStyles'
+import BlogPostFilter from '@/components/BlogPostFilter'
+import { blog } from '@/data/content'
 
-export const getStaticProps: GetStaticProps = async () => {
-  try {
-    const feed = await prisma.post.findMany({
-      where: { published: true },
-    })
-    return { props: { feed: JSON.parse(JSON.stringify(feed)), data: blog } }
-  } catch {
-    return { props: { feed: [] } }
-  }
-}
 
 const Blog = ({ data, feed }) => {
   
@@ -32,3 +21,11 @@ const Blog = ({ data, feed }) => {
 }
 
 export default Blog
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const feed = await prisma.post.findMany({ where: { published: true } })
+    return { props: { feed: JSON.parse(JSON.stringify(feed)), data: blog } }
+  } catch { return { props: { feed: [] } } }
+}
