7771c7ebee47c9f49d43dd32de1d8b8714aacd68
Amir Ardalan <amirrardalan@gmail.com>
2022-04-01 03:05:55 -0700

    Clean up

--- observable.d.ts
+++ observable.d.ts
@@ -1,10 +1,10 @@
-// Declaring Prism oneDark theme missing from @types/react-syntax-highlighter
+// Declare Prism oneDark theme missing from @types/react-syntax-highlighter
 
 declare module 'react-syntax-highlighter/dist/cjs/styles/prism' {
-  export { default as oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism/one-dark';
+  export { default as oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism/one-dark'
 }
 
 declare module 'react-syntax-highlighter/dist/cjs/styles/prism/one-dark' {
-  const style: any;
-  export default style;
+  const style: any
+  export default style
 }
