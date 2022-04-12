e9c61a64afcd61145ec1b405a966707f36bec203
Amir Ardalan <amirrardalan@gmail.com>
2022-04-12 15:26:06 -0700

    Set body parser to false

--- pages/api/revalidate/index.ts
+++ pages/api/revalidate/index.ts
@@ -13,3 +13,9 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
     return res.status(500).send('Error revalidating')
   }
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
