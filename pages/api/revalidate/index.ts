14e92955ab2ca4af3bf27fce48ebf754d625a070
Amir Ardalan <amirrardalan@gmail.com>
2022-04-12 12:05:04 -0700

    Fix env name

--- pages/api/revalidate/index.ts
+++ pages/api/revalidate/index.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.NEXT_PUBLIC_DEPLOY_TOKEN) {
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
