11d184358fcbd4d829c96a16c49f09e50275139c
Amir Ardalan <amirrardalan@gmail.com>
2022-04-16 02:30:13 -0700

    Utilize promises to prevent race conditions

--- lib/blog.ts
+++ lib/blog.ts
@@ -2,32 +2,34 @@ import Router from 'next/router'
 import revalidateChanges from '@/lib/revalidate'
 
 export async function publishPost(id: number, published: boolean, featured: boolean, latestPost: boolean): Promise<void> {
-  featured
-    ? await fetch(`/api/publish/${id}?published=${published}&featured=${featured}`, { method: 'PUT',})
-    : await fetch(`/api/publish/${id}?published=${published}`, { method: 'PUT',})
-  revalidateChanges(published, latestPost, featured)
+  if (featured) {
+    await fetch(`/api/publish/${id}?published=${published}&featured=${featured}`, { method: 'PUT',}).then(()=> {
+      revalidateChanges(published, latestPost, featured)
+    })
+  } else {
+    await fetch(`/api/publish/${id}?published=${published}`, { method: 'PUT',}).then(()=> {
+      revalidateChanges(published, latestPost, featured)
+    })
+  }
 }
 
 export async function editPost(slug: string): Promise<void> {
-  await fetch(`/blog/edit/${slug}`, {
-    method: 'PUT',
+  await fetch(`/blog/edit/${slug}`, { method: 'PUT' }).then(()=> {
+    Router.push(`/blog/edit/${slug}`)
   })
-  Router.push(`/blog/edit/${slug}`)
 }
 
 export async function deletePost(id: number, slug: string, published: boolean, redirect: string, latestPost: boolean, featured: boolean): Promise<void> {
-  await fetch(`/api/post/${id}`, {
-    method: 'DELETE',
-  })
-
-  Router.push(`/blog/edit/${slug}?delete`, undefined, { shallow: true }).then(()=> {
-    revalidateChanges(published, latestPost, featured)
-    if (published) {
-      Router.push(redirect)
-    }
-    else if (!published ) {
-      Router.push(redirect)
-      Router.reload()
-    }
+  await fetch(`/api/post/${id}`, { method: 'DELETE', }).then(()=> {
+    Router.push(`/blog/edit/${slug}?delete`, undefined, { shallow: true }).then(()=> {
+      revalidateChanges(published, latestPost, featured)
+      if (published) {
+        Router.push(redirect)
+      }
+      else if (!published ) {
+        Router.push(redirect)
+        Router.reload()
+      }
+    })
   })
 }
