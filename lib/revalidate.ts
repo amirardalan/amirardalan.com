5cd271f80380b941a7079062ad3f986ddd71475e
Amir Ardalan <amirrardalan@gmail.com>
2022-04-16 23:25:34 -0700

    Always revalidate current post

--- lib/revalidate.ts
+++ lib/revalidate.ts
@@ -3,17 +3,17 @@ import Router from 'next/router'
 const revalidateChanges = (published: boolean, latestPost: boolean, featured: boolean, setFetchStatus: any) => {
 
   const REVALIDATE_SECRET = process.env.NEXT_PUBLIC_REVALIDATE_SECRET
-  const isEditPage = Router.asPath.includes('/edit')
+  const isEditPage = Router.asPath.includes('/blog/edit/')
   const isDeleted = Router.asPath.includes('?delete')
   const revalidatePath = isEditPage ? Router.asPath.replace('/edit','') : Router.asPath
 
+  const post = fetch(`/api/revalidate?secret=${REVALIDATE_SECRET}&path=${revalidatePath}`)
   const blog = published ? fetch(`/api/revalidate?secret=${REVALIDATE_SECRET}&path=/blog`) : Promise.resolve()
-  const current = featured ? fetch(`/api/revalidate?secret=${REVALIDATE_SECRET}&path=${revalidatePath}`) : Promise.resolve()
   const home = featured || latestPost ? fetch(`/api/revalidate?secret=${REVALIDATE_SECRET}&path=/`) : Promise.resolve()
 
   setFetchStatus(true)
 
-  Promise.all([ current, blog, home ])
+  Promise.all([ post, blog, home ])
     .then(() => {
       if (isDeleted) return
 
