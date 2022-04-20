3db8f729e1b35411615ec9b3e0f04d4ea6b68f07
Amir Ardalan <amirrardalan@gmail.com>
2022-04-19 18:46:54 -0700

    Test paths

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -5,7 +5,7 @@ export const getServerSideProps = async ({ res }) => {
   
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
 
-  const staticPages = await globby([
+  const paths = await globby([
     '.next/server/pages/blog/**/*.json',
     '!.next/server/pages/**/*.js.nft.json',
     'pages/**/*{.tsx,.ts}',
@@ -14,7 +14,10 @@ export const getServerSideProps = async ({ res }) => {
     '!pages/api',
     '!pages/_*.tsx',
     '!pages/404.tsx',
+    '!*/documents/*',
   ])
+
+  console.log(paths)
 
   const feed = await prisma.post.findMany({
     where: { published: true },
@@ -23,8 +26,14 @@ export const getServerSideProps = async ({ res }) => {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${staticPages
-      .map((slug) => {
+    ${paths
+      .map((path) => {
+        let slug = path
+        .replace('.next/server/', '')
+        .replace('pages', '')
+        .replace('/index', '')
+        .replace('.tsx', '')
+        .replace('.json', '')
         return `
           <url>
             <loc>${slug}</loc>
