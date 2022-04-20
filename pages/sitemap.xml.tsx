245474c1d5da7409866696b0b9c5f45c966a752f
Amir Ardalan <amirrardalan@gmail.com>
2022-04-19 20:08:40 -0700

    Change to root dir

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -7,7 +7,7 @@ export const getServerSideProps = async ({ res }) => {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
 
   const staticPages = fs
-  .readdirSync('./pages')
+  .readdirSync('./')
   .filter((staticPage) => {
     return ![
       "_app.js",
