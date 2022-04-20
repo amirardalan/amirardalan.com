aaed1902de403685916cb16d1da8b7d075f896a9
Amir Ardalan <amirrardalan@gmail.com>
2022-04-19 21:43:46 -0700

    Log server/static

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -7,7 +7,7 @@ export const getServerSideProps = async ({ res }) => {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
 
   const staticPages = fs
-  .readdirSync('./.next/server/pages/')
+  .readdirSync('./.next/server/static/')
   console.log(staticPages)
   // .filter((staticPage) => {
   //   console.log(staticPage)
