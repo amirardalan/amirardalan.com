e0b6adb36839a265a9dd4d8edc4091c81e5da555
Amir Ardalan <amirrardalan@gmail.com>
2022-04-19 21:51:15 -0700

    Testing server/pages

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -7,7 +7,7 @@ export const getServerSideProps = async ({ res }) => {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
 
   const staticPages = fs
-  .readdirSync('./.next/static/')
+  .readdirSync('./.next/server/pages')
   console.log(staticPages)
   // .filter((staticPage) => {
   //   console.log(staticPage)
