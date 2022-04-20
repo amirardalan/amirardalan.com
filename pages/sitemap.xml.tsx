ad7fc4544c4a460f0af54870ac95982c4cf740e9
Amir Ardalan <amirrardalan@gmail.com>
2022-04-19 18:50:28 -0700

    Improve path filtering

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -5,7 +5,7 @@ export const getServerSideProps = async ({ res }) => {
   
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
 
-  const paths = await globby([
+  const pages = await globby([
     '.next/server/pages/blog/**/*.json',
     '!.next/server/pages/**/*.js.nft.json',
     'pages/**/*{.tsx,.ts}',
@@ -14,10 +14,9 @@ export const getServerSideProps = async ({ res }) => {
     '!pages/api',
     '!pages/_*.tsx',
     '!pages/404.tsx',
-    '!*/documents/*',
   ])
 
-  console.log(paths)
+  console.log(pages)
 
   const feed = await prisma.post.findMany({
     where: { published: true },
@@ -26,37 +25,35 @@ export const getServerSideProps = async ({ res }) => {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${paths
-      .map((path) => {
-        let slug = path
-        .replace('.next/server/', '')
-        .replace('pages', '')
-        .replace('/index', '')
-        .replace('.tsx', '')
-        .replace('.json', '')
+    ${pages
+      .map((page) => {
+        const path = page
+          .replace('.next/server/', '')
+          .replace('pages', '')
+          .replace('/index', '')
+          .replace('.tsx', '')
+          .replace('.json', '')
+        const route = path === '/index' ? '' : path;
         return `
           <url>
-            <loc>${slug}</loc>
-            <lastmod>${new Date().toISOString()}</lastmod>
-            <changefreq>monthly</changefreq>
-            <priority>1.0</priority>
+            <loc>${`${process.env.NEXT_PUBLIC_SITE_URL}${route}`}</loc>
           </url>
         `
       })
-      .join("")}
-    ${feed
-      .map(({ slug, editedAt }) => {
-        return `
-            <url>
-              <loc>${baseUrl}/documents/${slug}</loc>
-              <lastmod>${editedAt}</lastmod>
-              <changefreq>monthly</changefreq>
-              <priority>1.0</priority>
-            </url>
-          `
-      })
-      .join("")}
-  </urlset>
+      .join('')}
+      ${feed
+        .map(({ slug, editedAt }) => {
+          return `
+              <url>
+                <loc>${baseUrl}/documents/${slug}</loc>
+                <lastmod>${editedAt}</lastmod>
+                <changefreq>monthly</changefreq>
+                <priority>1.0</priority>
+              </url>
+            `
+          })
+        .join("")}
+    </urlset>
   `
 
   res.setHeader("Content-Type", "text/xml")
