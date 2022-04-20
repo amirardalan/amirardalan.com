3b00bfb7d68ff9e1223202f44857a51edd02f86f
Amir Ardalan <amirrardalan@gmail.com>
2022-04-19 23:08:46 -0700

    Read pages from server/pages

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -7,7 +7,7 @@ export const getServerSideProps = async ({ res }) => {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
 
   const staticPages = fs
-  .readdirSync('./.next/server/chunks')
+  .readdirSync('./.next/server/pages')
   .filter((staticPage) => {
     console.log(staticPage)
     return ![
