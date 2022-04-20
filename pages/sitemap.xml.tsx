4511548c053ef80f934c3596d9ab8070b24060e5
Amir Ardalan <amirrardalan@gmail.com>
2022-04-19 22:18:26 -0700

    Fix next dir path

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -7,7 +7,7 @@ export const getServerSideProps = async ({ res }) => {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
 
   const staticPages = fs
-  .readdirSync('./.next/static/chunks/pages')
+  .readdirSync('./__next/static/chunks/pages')
   .filter((staticPage) => {
     console.log(staticPage)
     return ![
