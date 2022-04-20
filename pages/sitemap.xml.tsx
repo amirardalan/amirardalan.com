dbfc3e78904ff336e9c5d857d7f2566ed1702a07
Amir Ardalan <amirrardalan@gmail.com>
2022-04-19 20:06:37 -0700

    Add fs and test pages dir

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,4 +1,4 @@
-import { globby } from 'globby'
+import fs from 'fs'
 import prisma from '@/lib/prisma'
 
 // Dynamically Generate sitemap.xml
@@ -6,57 +6,53 @@ export const getServerSideProps = async ({ res }) => {
   
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
 
-  const staticPages = await globby([
-    '!.next/server/pages/**/*.js.nft.json',
-    '!pages/blog/',
-    '!pages/api',
-    '!pages/_*.tsx',
-    '!pages/404.tsx',
-    '.next/server/pages/blog/**/*.{*}',
-    'pages/blog/index.tsx',
-    'pages{.tsx,.ts}',
-  ])
+  const staticPages = fs
+  .readdirSync('./pages')
+  .filter((staticPage) => {
+    return ![
+      "_app.js",
+      "_document.js",
+      "_error.js",
+      "sitemap.xml.js",
+    ].includes(staticPage);
+  })
+  .map((staticPagePath) => {
+    return `${baseUrl}/${staticPagePath}`;
+  });
 
   const feed = await prisma.post.findMany({
     where: { published: true },
     select: { slug: true, editedAt: true }
   })
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${staticPages
-      .map((staticPage) => {
-        const path = staticPage
-          .replace('.next/server/', '')
-          .replace('pages', '')
-          .replace('/index', '')
-          .replace('.tsx', '')
-          .replace('.json', '')
-        const slug = path === '/index' ? '' : path
-        console.log(slug)
+      .map((url) => {
         return `
           <url>
-            <loc>${`${baseUrl}${slug}`}</loc>
+            <loc>${url}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>1.0</priority>
           </url>
-        `
+        `;
       })
-      .join('')}
-
-      ${feed
-        .map(({ slug, editedAt }) => {
-          console.log(feed)
-          return `
-              <url>
-                <loc>${baseUrl}/documents/${slug}</loc>
-                <lastmod>${editedAt}</lastmod>
-                <changefreq>monthly</changefreq>
-                <priority>1.0</priority>
-              </url>
-            `
-          })
-        .join("")}
-    </urlset>
-  `
+      .join("")}
+    ${feed
+      .map(({ slug, editedAt }) => {
+        return `
+            <url>
+              <loc>${baseUrl}/${slug}</loc>
+              <lastmod>${editedAt}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+      })
+      .join("")}
+  </urlset>
+`
 
   res.setHeader("Content-Type", "text/xml")
   res.write(sitemap)
