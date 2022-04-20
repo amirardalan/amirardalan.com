9f88927459199029c7c54c6990d9046b4882877c
Amir Ardalan <amirrardalan@gmail.com>
2022-04-19 21:29:01 -0700

    Log everything up one dir

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -7,7 +7,7 @@ export const getServerSideProps = async ({ res }) => {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
 
   const staticPages = fs
-  .readdirSync('./.output/server/pages')
+  .readdirSync('./')
   .filter((staticPage) => {
     console.log(staticPage)
     return ![
