5f561cb7886e16c4492775a3c41312d68aa680dd
Amir Ardalan <amirrardalan@gmail.com>
2022-04-19 23:02:30 -0700

    Log server/chunks dir

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -7,7 +7,7 @@ export const getServerSideProps = async ({ res }) => {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
 
   const staticPages = fs
-  .readdirSync('./.next/server')
+  .readdirSync('./.next/server/chunks')
   .filter((staticPage) => {
     console.log(staticPage)
     return ![
