8591a490f01db2ee3686dc197676cf4e1a15724d
Amir Ardalan <amirrardalan@gmail.com>
2022-04-26 19:15:57 -0700

    Change JSX to TSX

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -9,13 +9,13 @@ import rangeParser from 'parse-numeric-range'
 
 import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
-import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx'
+import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx'
 import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript'
 import scss from 'react-syntax-highlighter/dist/cjs/languages/prism/scss'
 import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash'
 import markdown from 'react-syntax-highlighter/dist/cjs/languages/prism/markdown'
-import { join } from 'node:path/posix'
-SyntaxHighlighter.registerLanguage('jsx', jsx)
+
+SyntaxHighlighter.registerLanguage('tsx', tsx)
 SyntaxHighlighter.registerLanguage('typescript', typescript)
 SyntaxHighlighter.registerLanguage('scss', scss)
 SyntaxHighlighter.registerLanguage('bash', bash)
