11f31230b6626c2c2b065cff0801296d4b3263a2
Amir A <amirrardalan@gmail.com>
2022-08-04 23:31:54 -0700

    Update revalidate hook to stable version

--- pages/api/revalidate/index.ts
+++ pages/api/revalidate/index.ts
@@ -7,7 +7,7 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
   }
 
   try {
-    await res.unstable_revalidate(`${req.query.path}`)
+    await res.revalidate(`${req.query.path}`)
     return res.json({ revalidated: true })
   } catch (err) {
     return res.status(500).send('Error revalidating')
