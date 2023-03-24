48fa3245bc933c4c5edb8df6ff1ddf2691e6962e
Amir A <amirrardalan@gmail.com>
2023-03-23 18:41:33 -0700

    Add SWR cache invalidation

--- components/Like.tsx
+++ components/Like.tsx
@@ -1,5 +1,7 @@
 import { FC, useState, useEffect } from 'react';
+import useSWR from 'swr';
 import { likePost } from '@/lib/blog';
+import fetcher from '@/lib/fetcher';
 
 type LikeProps = {
   id: number;
@@ -8,14 +10,7 @@ type LikeProps = {
 
 const Like: FC<LikeProps> = ({ id, likes }) => {
   const [liked, setLiked] = useState(false);
-  const [likeCount, setLikeCount] = useState(liked ? likes + 1 : likes);
-
-  useEffect(() => {
-    const likedStatus = localStorage.getItem(`liked_${id}`);
-    if (likedStatus === 'true') {
-      setLiked(true);
-    }
-  }, [id]);
+  const [likeCount, setLikeCount] = useState(likes);
 
   const handleLike = async () => {
     const newLikeCount = liked ? Math.max(0, likeCount - 1) : likeCount + 1;
@@ -24,6 +19,24 @@ const Like: FC<LikeProps> = ({ id, likes }) => {
     await likePost(id, liked);
     localStorage.setItem(`liked_${id}`, (!liked).toString());
   };
+
+  const { data: updatedLikes } = useSWR(`/api/posts/${id}/likes`, fetcher, {
+    refreshInterval: 5000,
+    revalidateOnFocus: false,
+  });
+
+  useEffect(() => {
+    const likedStatus = localStorage.getItem(`liked_${id}`);
+    if (likedStatus === 'true') {
+      setLiked(true);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (updatedLikes !== undefined) {
+      setLikeCount(updatedLikes.likes);
+    }
+  }, [updatedLikes]);
 
   return (
     <button onClick={handleLike}>
