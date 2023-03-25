7394b3fbeb4fcf2a5595a17e272ab3dca3eacd4c
Amir Ardalan <amirrardalan@gmail.com>
2023-03-24 19:05:20 -0700

    Improve support component

--- components/BlogSupport.tsx
+++ components/BlogSupport.tsx
@@ -66,15 +66,20 @@ const BlogSupport: FC<BlogSupportProps> = ({ id }) => {
         width: '100%',
         display: 'flex',
         position: 'relative',
-        padding: '2.8rem',
+        padding: '3rem',
+        '@media(max-width: 480px)': {
+          padding: '3rem 2rem',
+        },
       },
       '.supportOptions': {
         display: 'flex',
         flexDirection: 'column',
-        margin: '4.5rem 2.5rem',
+        margin: '2.5rem',
+        '@media (max-width: 480px)': {
+          margin: '1.6rem 2rem',
+        },
         '.optionsHeading': {
           h4: {
-            fontSize: 12,
             textTransform: 'uppercase',
             fontFamily: 'var(--font-primary)',
             fontWeight: 'normal',
@@ -99,15 +104,14 @@ const BlogSupport: FC<BlogSupportProps> = ({ id }) => {
         '.paymentMethods': {
           position: 'relative',
           display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-around',
+          flexDirection: 'column',
           '.ether': {
             display: 'flex',
             background: 'var(--icon-eth) no-repeat',
             backgroundSize: 'contain',
             height: 33,
             width: 133,
+            marginBottom: '1rem',
           },
           '.paypal': {
             display: 'flex',
@@ -119,7 +123,15 @@ const BlogSupport: FC<BlogSupportProps> = ({ id }) => {
           '.copyConfirmation': {
             display: 'flex',
             flexDirection: 'column',
-            marginTop: '2rem',
+            marginTop: '2.4rem',
+            '@media (max-width: 980px)': {
+              marginLeft: '2rem',
+            },
+          },
+          '.qrCode': {
+            '@media (max-width: 980px)': {
+              display: 'none',
+            },
           },
           '.ethAddressCopied': {
             zIndex: 3,
@@ -128,6 +140,7 @@ const BlogSupport: FC<BlogSupportProps> = ({ id }) => {
             flexDirection: 'row',
             justifyContent: 'space-between',
             position: 'absolute',
+            top: -26,
             left: -25,
             width: '100%',
             alignItems: 'center',
@@ -141,21 +154,18 @@ const BlogSupport: FC<BlogSupportProps> = ({ id }) => {
               fontSize: 12,
               fontFamily: 'var(--font-primary)',
             },
+            '@media(max-width: 480px)': {
+              top: -12,
+            },
           },
         },
       },
-    },
-    '.left': {
-      width: '70%',
     },
     '.donate': {
       display: 'flex',
       justifyContent: 'flex-start',
       span: {
         marginRight: '.5rem',
-      },
-      a: {
-        color: 'var(--color-text)',
       },
     },
     '.copyContainer': {
@@ -167,6 +177,9 @@ const BlogSupport: FC<BlogSupportProps> = ({ id }) => {
       h4: {
         fontSize: 22,
         marginRight: '1rem',
+        '@media (max-width: 480px)': {
+          fontSize: 18,
+        },
       },
       p: {
         fontSize: 14,
@@ -183,7 +196,7 @@ const BlogSupport: FC<BlogSupportProps> = ({ id }) => {
       },
     },
     '@media (max-width: 768px)': {
-      display: 'none', // suppress on mobile
+      // display: 'none', // suppress on mobile
     },
   });
 
@@ -199,11 +212,21 @@ const BlogSupport: FC<BlogSupportProps> = ({ id }) => {
             <div className="left">
               <div className="copyContainer">
                 <span className="supportHeading">
-                  <h4>Like this article:</h4>
+                  <h4>Did you enjoy this post?</h4>
                   <LikeButton id={id} />
                 </span>
                 <div className="donate">
-                  <span>☕</span>
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="18"
+                      height="18"
+                      fill="var(--color-text)"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12.874 6.999c4.737-4.27-.979-4.044.116-6.999-3.781 3.817 1.41 3.902-.116 6.999zm-2.78.001c3.154-2.825-.664-3.102.087-5.099-2.642 2.787.95 2.859-.087 5.099zm8.906 2.618c-.869 0-1.961-.696-1.961-1.618h-10.039c0 .921-1.13 1.618-2 1.618v1.382h14v-1.382zm-13 2.382l2.021 12h7.959l2.02-12h-12z" />
+                    </svg>
+                  </span>
                   <a onClick={handleshowOptions}>Buy me a coffee</a>
                 </div>
               </div>
@@ -232,9 +255,7 @@ const BlogSupport: FC<BlogSupportProps> = ({ id }) => {
               <a onClick={() => handleCopyAddress()}>
                 <div className="ether" />
               </a>
-              <div className="optionsHeading">
-                <h4>Select payment type</h4>
-              </div>
+
               <a
                 href="https://www.paypal.com/donate/?hosted_button_id=PDSXCAVYMLW2G"
                 target="_blank"
--- components/LikeIcon.tsx
+++ components/LikeIcon.tsx
@@ -11,71 +11,37 @@ const LikeIcon: FC<LikeIconProps> = ({ active }) => {
   const iconHeight = 22;
   const iconWidth = 22;
 
-  if (theme.active === 'dark') {
-    return (
-      <>
-        {active ? (
-          <svg
-            width={iconWidth}
-            height={iconHeight}
-            viewBox="0 0 139 139"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M103.48 20.2303L127.417 40.1767V69.8706L69.5 118.133L11.5833 69.8648V40.1767L35.5203 20.2303L69.5 42.8815L103.48 20.2303ZM104.25 5.79163L69.5 28.9583L34.75 5.79163L0 34.75V75.2916L69.5 133.208L139 75.2916V34.75L104.25 5.79163Z"
-              fill="#E4E9F8"
-            />
-          </svg>
-        ) : (
-          <svg
-            width={iconWidth}
-            height={iconHeight}
-            viewBox="0 0 139 139"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M104.25 5.79163L69.5 28.9583L34.75 5.79163L0 34.75V75.2916L69.5 133.208L139 75.2916V34.75L104.25 5.79163Z"
-              fill="#3DFFC5"
-            />
-          </svg>
-        )}
-      </>
-    );
-  } else {
-    return (
-      <>
-        {active ? (
-          <svg
-            width={iconWidth}
-            height={iconHeight}
-            viewBox="0 0 139 139"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M103.48 20.2303L127.417 40.1767V69.8706L69.5 118.133L11.5833 69.8648V40.1767L35.5203 20.2303L69.5 42.8815L103.48 20.2303ZM104.25 5.79163L69.5 28.9583L34.75 5.79163L0 34.75V75.2916L69.5 133.208L139 75.2916V34.75L104.25 5.79163Z"
-              fill="#262738"
-            />
-          </svg>
-        ) : (
-          <svg
-            width={iconWidth}
-            height={iconHeight}
-            viewBox="0 0 139 139"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M104.25 5.79163L69.5 28.9583L34.75 5.79163L0 34.75V75.2916L69.5 133.208L139 75.2916V34.75L104.25 5.79163Z"
-              fill="#7E37A4"
-            />
-          </svg>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      {active ? (
+        <svg
+          width={iconWidth}
+          height={iconHeight}
+          viewBox="0 0 139 139"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M103.48 20.2303L127.417 40.1767V69.8706L69.5 118.133L11.5833 69.8648V40.1767L35.5203 20.2303L69.5 42.8815L103.48 20.2303ZM104.25 5.79163L69.5 28.9583L34.75 5.79163L0 34.75V75.2916L69.5 133.208L139 75.2916V34.75L104.25 5.79163Z"
+            fill="var(--color-text)"
+          />
+        </svg>
+      ) : (
+        <svg
+          width={iconWidth}
+          height={iconHeight}
+          viewBox="0 0 139 139"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M104.25 5.79163L69.5 28.9583L34.75 5.79163L0 34.75V75.2916L69.5 133.208L139 75.2916V34.75L104.25 5.79163Z"
+            fill="var(--color-primary)"
+          />
+        </svg>
+      )}
+    </>
+  );
 };
 
 export default LikeIcon;
