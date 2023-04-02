5726d1dd0b22e91563642e33cfcb968e0ee77e12
Amir A <amirrardalan@gmail.com>
2023-04-01 17:01:00 -0700

    Upload images using upload_stream

--- pages/api/upload/index.ts
+++ pages/api/upload/index.ts
@@ -2,13 +2,13 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import multer from 'multer';
 import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
 
-const upload = multer();
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 export const config = {
   api: {
@@ -28,21 +28,29 @@ const uploadImageHandler = async (
       });
     });
 
-    const file = req.file.path;
-    console.log('imageFile: ', file);
+    const fileBuffer = req.file.buffer;
 
-    const result: UploadApiResponse = await cloudinary.uploader.upload(file, {
-      folder: 'blog-images',
-    });
-
-    res.status(200).json({
-      success: true,
-      data: {
-        url: result.secure_url,
-        publicId: result.public_id,
-      },
-      message: 'File uploaded successfully',
-    });
+    const stream = cloudinary.uploader.upload_stream(
+      { folder: 'Blog' },
+      (error: any, result: any) => {
+        if (error) {
+          console.error(error);
+          res
+            .status(500)
+            .json({ success: false, message: 'Error uploading image' });
+        } else {
+          res.status(200).json({
+            success: true,
+            data: {
+              url: result.secure_url,
+              publicId: result.public_id,
+            },
+            message: 'File uploaded successfully',
+          });
+        }
+      }
+    );
+    stream.end(fileBuffer);
   } catch (err) {
     console.error(err);
     res.status(500).json({ success: false, message: 'Error uploading image' });
