7eced4b55a0cf82c2af2bd0d912bc858410779ed
Amir Ardalan <amirrardalan@gmail.com>
2023-04-16 17:36:35 -0700

    Add example

--- utils/convertUrlToMarkdown.ts
+++ utils/convertUrlToMarkdown.ts
@@ -1,3 +1,5 @@
+// Convert an image URL to markdown (e.g. https://example.com/image.png -> ![Image](https://example.com/image.png)
+
 export const convertUrlToMarkdown = (url: string): string => {
   const imageName = url
     .replace(/^.*\//, '')
