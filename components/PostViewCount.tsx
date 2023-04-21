030c9762ec766a725d699d1ee12adb0877001738
Amir A <amirrardalan@gmail.com>
2023-04-20 20:31:23 -0700

    Add view count loading animation

--- components/PostViewCount.tsx
+++ components/PostViewCount.tsx
@@ -1,20 +1,39 @@
 import { FC } from 'react';
 import usePostViewCount from '@/hooks/usePostViewCount';
 import formatNumber from '@/utils/formatNumber';
+import { css } from '@emotion/react';
 
 type PostViewCountProps = {
   slug: string;
 };
 
 const PostViewCount: FC<PostViewCountProps> = ({ slug }) => {
-  const { postviews } = usePostViewCount(slug);
-
+  const { postviews, isValidating } = usePostViewCount(slug);
   const views = postviews ? postviews : 0;
 
+  const styleViews = css({
+    position: 'relative',
+    height: 14,
+    width: 100,
+    overflow: 'hidden',
+    marginLeft: '.5rem',
+    '.views': {
+      transition: 'transform .2s ease-in-out, opacity .5s ease-in-out',
+      position: 'absolute',
+      transform: `translateY(${isValidating ? 10 : 0}px)`,
+      opacity: `${isValidating ? '0' : '1'}`,
+    },
+  });
+
   return (
-    <span>
-      • {formatNumber(Number(views))} {postviews === 1 ? 'view' : 'views'}
-    </span>
+    <>
+      <span>•</span>
+      <div css={styleViews}>
+        <span className="views">
+          {formatNumber(Number(views))} {postviews === 1 ? 'view' : 'views'}
+        </span>
+      </div>
+    </>
   );
 };
 
