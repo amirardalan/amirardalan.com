45d8e4d41532a35c8d76f4327f53205faa2b2a63
Amir Ardalan <amirrardalan@gmail.com>
2023-04-23 18:02:21 -0700

    Update favicons

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -2,12 +2,12 @@ export const themeLight = {
   active: 'light',
   favicon: '/favicon-light.png',
   gh: 'dark',
-  canvas: '#a3caec',
+  canvas: '#89a9c4',
 };
 
 export const themeDark = {
   active: 'dark',
   favicon: '/favicon-dark.png',
   gh: 'light',
-  canvas: '#9e7dfe',
+  canvas: '#886fd4',
 };
