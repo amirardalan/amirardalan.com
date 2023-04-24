eab18ea72501c87c2880f29a59470180ea176ec6
Amir Ardalan <amirrardalan@gmail.com>
2023-04-23 19:08:44 -0700

    Update og image styles

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -45,7 +45,7 @@ export default async function handler(req: Request) {
                 {description}
               </div>
             </div>
-            <div tw="border-t-2 border-[#634da5] text-4xl font-light absolute bottom-12 right-20 text-right">
+            <div tw="border-t-2 border-[#386B9E] text-4xl font-light absolute bottom-12 right-20 text-right">
               {displayUrl}
             </div>
           </div>
