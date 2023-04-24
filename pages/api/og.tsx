72681d219a303b45900795d420abe6566a22e84c
Amir Ardalan <amirrardalan@gmail.com>
2023-04-23 17:29:59 -0700

    Improve colors

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -45,7 +45,7 @@ export default async function handler(req: Request) {
                 {description}
               </div>
             </div>
-            <div tw="border-t-2 border-[#406082] text-4xl font-light absolute bottom-12 right-20 text-right">
+            <div tw="border-t-2 border-[#634da5] text-4xl font-light absolute bottom-12 right-20 text-right">
               {displayUrl}
             </div>
           </div>
