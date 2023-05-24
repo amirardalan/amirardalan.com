4bce006cf9e18d38e40b1f7d90124bca5f0d4651
Amir Ardalan <amirrardalan@gmail.com>
2023-05-23 21:09:38 -0700

    Clean up

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,13 +1,11 @@
 export const themeLight = {
   active: 'light',
   favicon: '/favicon-light.png',
-  gh: 'dark',
   canvas: '#9f9f9f',
 };
 
 export const themeDark = {
   active: 'dark',
   favicon: '/favicon-dark.png',
-  gh: 'light',
   canvas: '#9f9f9f',
 };
