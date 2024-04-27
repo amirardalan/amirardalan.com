a6f9ab5ebc362597fabaedc7218d36ef56363e9d
Amir Ardalan <hi@amirardalan.com>
2024-04-27 13:49:24 -0700

    Utilize cloudinary config

--- lib/photos.ts
+++ lib/photos.ts
@@ -1,11 +1,6 @@
-import { v2 as cloudinary } from 'cloudinary';
+import cloudinary from '@/lib/cloudinaryConfig';
 
 const fetchPhotos = async () => {
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
 
   const res = await cloudinary.api.resources({
     type: 'upload',
