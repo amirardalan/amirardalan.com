efc03429b9ed11ff48346560106f7859bb11bf2c
Amir Ardalan <hi@amirardalan.com>
2024-04-27 23:07:41 -0700

    Move to env var

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
       },
       {
         source: '/resume',
-        destination: 'https://s3.us-west-2.amazonaws.com/amirardalan.com/amir-ardalan-resume.pdf',
+        destination: process.env.RESUME_URL,
         permanent: true,
       },
     ];
