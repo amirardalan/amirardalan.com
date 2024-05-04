580e7727b7cf8d4600636ebc17a017c9ab36c2d0
Amir Ardalan <hi@amirardalan.com>
2024-05-03 22:35:34 -0700

    Temporarily bypass signed request

--- pages/api/photos/index.ts
+++ pages/api/photos/index.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import revalidateChanges from '@/lib/revalidate';
-import { signatureHelper } from '@/lib/signatureHelper';
 
 export const config = {
   api: {
@@ -8,28 +7,21 @@ export const config = {
   },
 };
 
-const secretKey = process.env.CLOUDINARY_API_SECRET || '';
-const signatureHeader = 'x-cloudinary-signature';
+const photosHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const { notification_type } = req.body;
 
-const photosHandler = signatureHelper(
-  secretKey,
-  signatureHeader,
-  async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === 'POST') {
-      const { notification_type } = req.body;
-
-      if (notification_type === 'upload' || notification_type === 'delete') {
-        revalidateChanges('photos');
-        res.status(200).json({ message: 'Revalidation triggered' });
-      } else {
-        res.status(400).json({
-          message: `Unsupported notification type: ${notification_type}`,
-        });
-      }
+    if (notification_type === 'upload' || notification_type === 'delete') {
+      revalidateChanges('photos');
+      res.status(200).json({ message: 'Revalidation triggered' });
     } else {
-      res.status(405).json({ message: 'Method Not Allowed' });
+      res.status(400).json({
+        message: `Unsupported notification type: ${notification_type}`,
+      });
     }
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
-);
+};
 
 export default photosHandler;
