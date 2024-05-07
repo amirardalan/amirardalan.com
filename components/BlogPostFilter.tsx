56f657ab090ed38a9e85551dc5c3f9a75dbd0255
Amir Ardalan <hi@amirardalan.com>
2024-05-06 17:50:27 -0700

    Fix likes count

--- components/BlogPostFilter.tsx
+++ components/BlogPostFilter.tsx
@@ -178,7 +178,7 @@ const BlogPostFilter: FC<BlogPostFilterProps> = ({ blog, feed }) => {
   }
 
   const { totalLikesCount } = useTotalLikes();
-  const averageLikes = totalLikesCount.likes / feed.length;
+  const averageLikes = totalLikesCount / feed.length;
 
   interface PostFeedItem extends PostProps, FeedItem {}
 
