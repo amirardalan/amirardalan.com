5916eea67b5ab521d9a9180736ed82bc1aa62070
amirardalan <hi@amir.sh>
2025-03-01 22:34:01 -0800

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,29 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const { NEXT_PUBLIC_APP_URL } = process.env;
+  const { NEXT_PUBLIC_URL } = process.env;
 
   return [
     {
-      url: `${NEXT_PUBLIC_APP_URL}`,
+      url: `${NEXT_PUBLIC_URL}`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: `${NEXT_PUBLIC_APP_URL}/about`,
+      url: `${NEXT_PUBLIC_URL}/about`,
       lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${NEXT_PUBLIC_URL}/uses`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${NEXT_PUBLIC_URL}/account`,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
