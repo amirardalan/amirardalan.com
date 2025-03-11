2101d3b5dac9f19b29702dec39bef141c7433011
amirardalan <hi@amir.sh>
2025-03-10 22:48:35 -0700

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from 'next';
 import createMDX from '@next/mdx';
-// import postgres from 'postgres';
-
-// export const sql = postgres(process.env.DATABASE_URL!, {
-//   ssl: 'allow',
-// });
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -17,24 +12,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // async redirects() {
-  //   if (!process.env.DATABASE_URL) {
-  //     return [];
-  //   }
-
-  //   let redirects = await sql`
-  //     SELECT source, destination, permanent
-  //     FROM redirects;
-  //   `;
-
-  //   return redirects.map(({ source, destination, permanent }) => ({
-  //     source,
-  //     destination,
-  //     permanent: !!permanent,
-  //   }));
-  // },
-  // Note: Using the Rust compiler means we cannot use
-  // rehype or remark plugins. For my app, this is fine.
   experimental: {
     mdxRs: true,
   },
