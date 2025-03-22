005ff0651e8f809c7eb05d01be47b9168f34cf93
Amir Ardalan <hi@amirardalan.com>
2025-03-22 15:25:24 -0700

    Update content

--- data/metadata.ts
+++ data/metadata.ts
@@ -2,6 +2,6 @@ export const metadata = {
   name: 'Amir Ardalan',
   title: 'Amir Ardalan – Developer & Designer',
   description:
-    'Software Engineer & UI Designer with 10+ years of experience creating innovative digital solutions.',
+    'Frontend Engineer & UI Designer with 10+ years of experience creating innovative digital solutions.',
   xHandle: '@amirardalan',
 };
