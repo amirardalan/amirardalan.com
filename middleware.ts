dc93d573703d8a8c21f9a5e93b6147a1295c123d
Amir Ardalan <hi@amirardalan.com>
2025-03-23 13:15:44 -0700

    Update middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -15,5 +15,16 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - _next/data (data requests)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    '/((?!api|_next/static|_next/image|_next/data|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)',
+  ],
 };
