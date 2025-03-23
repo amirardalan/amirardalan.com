1e77feedfd934a280018cabe315e669c2779c250
amirardalan <hi@amir.sh>
2025-03-22 21:09:00 -0700

    Consolidate blog landing page code

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,13 +1,24 @@
-import BlogPosts from '@/components/blog/BlogPosts';
+import { createClient } from '@/utils/supabase/server';
 
-export default function Blog() {
+export default async function Blog() {
+  const supabase = await createClient();
+  const { data: posts } = await supabase.from('Post').select();
+
   return (
     <main>
       <div className="mt-8">
         <h2 className="mb-6 border-b-2 border-solid border-gray-300 pb-4 text-xxl text-dark dark:border-gray-600 dark:text-light">
           Blog
         </h2>
-        <BlogPosts />
+        <div className="text-dark dark:text-light">
+          <ul>
+            {posts?.map((post) => (
+              <li key={post.id}>
+                <a href={`/blog/${post.slug}`}>{post.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </main>
   );
