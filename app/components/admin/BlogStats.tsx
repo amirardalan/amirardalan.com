9713778b21a179a0ad21aaa9395053414e279468
amirardalan <hi@amir.sh>
2025-03-24 14:53:45 -0700

    Improve admin dashboard layout

--- app/components/admin/BlogStats.tsx
+++ app/components/admin/BlogStats.tsx
@@ -1,3 +1,9 @@
 export default function BlogStats() {
-  return <div>Blog Stats Graphs Here</div>;
+  return (
+    <div className="mb-4 grid w-full grid-cols-1 gap-6 md:grid-cols-1 lg:grid-cols-1">
+      <div className="rounded-lg border border-zinc-200 p-6 dark:border-zinc-700">
+        Blog stats go here.
+      </div>
+    </div>
+  );
 }
