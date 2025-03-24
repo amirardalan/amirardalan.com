fe183529c103c88a96ac6a923926fb2c9a8bd217
amirardalan <hi@amir.sh>
2025-03-24 15:25:40 -0700

    Add about links

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,8 @@ export default function About() {
         <PageHeading title={'About'} />
         <div className="my-10 max-w-7xl font-serif text-3xl leading-relaxed">
           <p className="mb-6 text-dark dark:text-light">
-            Design Engineer with 10+ years of experience based in Portland, OR.
+            Frontend Engineer with 10+ years of experience based in Portland,
+            OR.
           </p>
           <p className="mb-6 text-dark dark:text-light">
             I&apos;ve collaborated with renowned brands including Nike,
@@ -20,22 +21,47 @@ export default function About() {
             that delight users and drive results.
           </p>
         </div>
-        <span className="mt-2 flex flex-row">
-          <p className="text-zinc-400 dark:text-zinc-500">
-            Built and maintained by
-          </p>
-          <Tooltip text="x.com" pos="b">
-            <a
-              href="https://x.com/amirardalan"
-              rel="noopener noreferrer"
-              target="_blank"
-              className="ml-1 text-primary"
-            >
-              @amirardalan
-            </a>
-          </Tooltip>
-          <p className="text-dark dark:text-light">.</p>
-        </span>
+        <div className="mt-2 flex flex-col">
+          <div className="flex flex-row">
+            <p className="text-zinc-400 dark:text-zinc-500">x:</p>
+            <Tooltip text="x.com" pos="r">
+              <a
+                href="https://x.com/amirardalan"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="ml-1 text-primary"
+              >
+                @amirardalan
+              </a>
+            </Tooltip>
+          </div>
+          <div className="flex flex-row">
+            <p className="text-zinc-400 dark:text-zinc-500">linkedin:</p>
+            <Tooltip text="linkedin.com" pos="r">
+              <a
+                href="https://linkedin.com/in/amirardalan"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="ml-1 text-primary"
+              >
+                /in/amirardalan
+              </a>
+            </Tooltip>
+          </div>
+          <div className="flex flex-row">
+            <p className="text-zinc-400 dark:text-zinc-500">resume:</p>
+            <Tooltip text="resume" pos="r">
+              <a
+                href="https://amir.sh/resume"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="ml-1 text-primary"
+              >
+                download
+              </a>
+            </Tooltip>
+          </div>
+        </div>
       </div>
     </main>
   );
