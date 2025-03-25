006e2fde69a31218e08bd126dc9ca1ada7add84e
amirardalan <hi@amir.sh>
2025-03-25 11:03:20 -0700

    Update on-demand revalidation

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,21 +1,26 @@
-import { revalidatePath } from 'next/cache';
+import { revalidateTag } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
   try {
-    // Get the secret token from the request
     const token = request.headers.get('x-revalidate-token');
-    const { path = '/' } = await request.json();
+    const { tag } = await request.json();
 
-    // Check if the token is valid
     if (token !== process.env.REVALIDATE_TOKEN) {
       return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
     }
 
-    // Revalidate the specific path
-    revalidatePath(path);
+    if (!tag) {
+      return NextResponse.json(
+        { message: 'Tag is required for revalidation' },
+        { status: 400 }
+      );
+    }
 
-    return NextResponse.json({ revalidated: true, path }, { status: 200 });
+    // Revalidate the specified tag
+    revalidateTag(tag);
+
+    return NextResponse.json({ revalidated: true, tag }, { status: 200 });
   } catch (error) {
     console.error('Error during revalidation:', error);
     return NextResponse.json(
