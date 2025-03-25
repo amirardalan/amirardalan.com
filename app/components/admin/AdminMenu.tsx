776602a4aca30fc874447fc456d60324d0523512
amirardalan <hi@amir.sh>
2025-03-24 18:35:14 -0700

    Improve admin menu

--- app/components/admin/AdminMenu.tsx
+++ app/components/admin/AdminMenu.tsx
@@ -2,6 +2,8 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import IconBlogControls from '@/components/icons/IconBlogControls';
+import IconAccount from '@/components/icons/IconAccount';
 
 export default function AdminMenu() {
   const pathname = usePathname();
@@ -17,35 +19,43 @@ export default function AdminMenu() {
   };
 
   return (
-    <nav className="flex flex-wrap space-x-4 rounded-lg bg-zinc-200 p-4 text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
-      <Link href="/admin" className={getLinkClass('/admin')}>
-        Dashboard
-      </Link>
-      <Link href="/admin/blog/new" className={getLinkClass('/admin/blog/new')}>
-        New Post
-      </Link>
-      <Link
-        href="/admin/blog/drafts"
-        className={getLinkClass('/admin/blog/drafts')}
-      >
-        Drafts
-      </Link>
-      <Link
-        href="/admin/blog/published"
-        className={getLinkClass('/admin/blog/published')}
-      >
-        Published
-      </Link>
-      <span>|</span>
-      <Link href="/admin/account" className={getLinkClass('/admin/account')}>
-        Account
-      </Link>
-      <Link
-        href="/api/auth/signout?callbackUrl=/&redirect=false"
-        className={getLinkClass('/api/auth/signout')}
-      >
-        Sign Out
-      </Link>
+    <nav className="flex items-center justify-between rounded-lg bg-zinc-200 px-4 py-3 font-mono text-xs uppercase text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
+      <div className="flex flex-wrap space-x-4">
+        <IconBlogControls />
+        <Link href="/admin" className={getLinkClass('/admin')}>
+          Dashboard
+        </Link>
+        <Link
+          href="/admin/blog/new"
+          className={getLinkClass('/admin/blog/new')}
+        >
+          New Post
+        </Link>
+        <Link
+          href="/admin/blog/drafts"
+          className={getLinkClass('/admin/blog/drafts')}
+        >
+          Drafts
+        </Link>
+        <Link
+          href="/admin/blog/published"
+          className={getLinkClass('/admin/blog/published')}
+        >
+          Published
+        </Link>
+      </div>
+      <div className="flex flex-wrap space-x-4">
+        <IconAccount />
+        <Link href="/admin/account" className={getLinkClass('/admin/account')}>
+          Account
+        </Link>
+        <Link
+          href="/api/auth/signout?callbackUrl=/&redirect=false"
+          className={getLinkClass('/api/auth/signout')}
+        >
+          Sign Out
+        </Link>
+      </div>
     </nav>
   );
 }
