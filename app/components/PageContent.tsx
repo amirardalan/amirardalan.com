6c15841bb708e11770cde47533f3c381cb3a98e3
amirardalan <hi@amir.sh>
2025-03-24 20:32:01 -0700

    Improve homepage styles

--- app/components/PageContent.tsx
+++ app/components/PageContent.tsx
@@ -7,9 +7,13 @@ import TypingAnimation from '@/components/content/TypingAnimation';
 export default function PageContent() {
   return (
     <div className="flex w-full flex-col">
+      <div className="items-left mt-12 flex justify-start space-x-2 text-left text-xl">
+        <span className="text-primary">{'> ~ %'}</span>
+        <TypingAnimation />
+      </div>
       <div className="mt-8 flex flex-col">
         <div className="text-s mt-5 font-sans text-gray-600 dark:text-gray-500">
-          Frontend Engineer & UI/UX Expert
+          Frontend Engineer & UI/UX Designer
         </div>
         <div className="mt-8 flex space-x-4">
           <Link href="/blog">
@@ -18,12 +22,6 @@ export default function PageContent() {
           <Link href="/about">
             <Button text="About" variant="secondary" size="large" />
           </Link>
-        </div>
-      </div>
-      <div>
-        <div className="items-left mt-4 flex justify-start space-x-2 py-8 text-left text-xl">
-          <span className="text-primary">{'> ~ %'}</span>
-          <TypingAnimation />
         </div>
       </div>
     </div>
