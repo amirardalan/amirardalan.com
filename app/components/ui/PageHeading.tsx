e80831ec6eae4bc1f5faa9110a00f1128a0dab90
Amir Ardalan <hi@amirardalan.com>
2025-03-25 22:42:04 -0700

    Improve page heading component styles

--- app/components/ui/PageHeading.tsx
+++ app/components/ui/PageHeading.tsx
@@ -10,7 +10,7 @@ export default function PageHeading({ title }: PageHeadingProps) {
   const pathname = usePathname();
 
   return (
-    <div className="text-md mb-6 flex w-full border-b-2 border-zinc-400 pb-4 text-dark dark:border-zinc-600 dark:text-light">
+    <div className="text-md mb-12 flex w-full border-b-2 border-zinc-300 pb-8 text-dark dark:border-zinc-700 dark:text-light">
       <h1>{title}</h1> <span className="ml-1"> — Amir Ardalan</span>
     </div>
   );
