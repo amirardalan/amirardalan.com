fe7fec33e252558118563c974bf7f2c752c3fb1a
Amir Ardalan <hi@amirardalan.com>
2025-03-25 22:46:30 -0700

    Remove unused import

--- app/components/ui/PageHeading.tsx
+++ app/components/ui/PageHeading.tsx
@@ -1,14 +1,10 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
-
 type PageHeadingProps = {
   title: string;
 };
 
 export default function PageHeading({ title }: PageHeadingProps) {
-  const pathname = usePathname();
-
   return (
     <div className="text-md mb-12 flex w-full border-b-2 border-zinc-300 pb-8 text-dark dark:border-zinc-700 dark:text-light">
       <h1>{title}</h1> <span className="ml-1"> — Amir Ardalan</span>
