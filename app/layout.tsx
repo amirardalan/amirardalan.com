bd0cda26267732280b420f4c60f8845274019c0c
Amir Ardalan <hi@amirardalan.com>
2025-03-28 18:59:23 -0700

    Improve styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,7 @@ export default async function RootLayout({
         >
           <div className="flex min-h-screen flex-col px-6 pt-4 lg:px-10 lg:pt-8">
             <Header />
-            <div>{children}</div>
+            <div className="h-screen">{children}</div>
             <Footer />
           </div>
         </body>
