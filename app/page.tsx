19e8f28679502b9fd40f1a97385f1638e0fcc192
Amir Ardalan <hi@amirardalan.com>
2025-03-28 23:44:12 -0700

    Fix canvas tooltip scroll bug

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import CanvasLoader from '@/components/content/CanvasLoader';
 
 export default function Home() {
   return (
-    <div className="relative flex h-[calc(100vh-160px)] flex-1">
+    <div className="relative flex h-[calc(100vh-85px)] flex-1 overflow-hidden">
       <CanvasLoader />
       <HomeContent />
     </div>
