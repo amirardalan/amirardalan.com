a65d4596be52b2ba4f8bf116e79d5760c33c066d
Amir Ardalan <hi@amirardalan.com>
2025-03-29 20:20:39 -0700

    Update og image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -14,17 +14,17 @@ export const contentType = 'image/png';
 // Image generation
 export default async function Image() {
   const dmSerif = await readFile(
-    join(process.cwd(), 'public/fonts/dm-serif-text-v12-latin-regular.ttf')
+    join(process.cwd(), 'public/fonts/jura-v31-latin-regular.ttf')
   );
 
   return new ImageResponse(
     (
       // ImageResponse JSX element
-      <div tw="flex h-full w-full items-center justify-center">
+      <div tw="flex h-full w-full items-center justify-center bg-zinc-900">
         <div tw="flex p-8">
           <div tw="flex flex-col md:flex-row w-full md:items-center justify-between p-8">
             <h2 tw="flex flex-col text-[124px] text-white text-left">
-              <span>Site</span>
+              <span>amir.sh</span>
             </h2>
             <div tw="flex w-132 ml-6">
               <h3 tw="text-4xl text-zinc-500 leading-10">
