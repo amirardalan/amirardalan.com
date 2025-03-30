b4d03a2b9eaaec39a9e6b819cb244b244bbff20a
amirardalan <hi@amir.sh>
2025-03-29 19:37:26 -0700

    Remove tw util from og image causing issues

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -20,7 +20,7 @@ export default async function Image() {
   return new ImageResponse(
     (
       // ImageResponse JSX element
-      <div tw="flex h-full w-full items-center justify-center bg-zinc-950">
+      <div tw="flex h-full w-full items-center justify-center">
         <div tw="flex p-8">
           <div tw="flex flex-col md:flex-row w-full md:items-center justify-between p-8">
             <h2 tw="flex flex-col text-[124px] text-white text-left">
