236ab366a9f3863169d5205efa28496fd3d52767
amirardalan <hi@amir.sh>
2025-03-31 22:40:48 -0700

    Style blog support component

--- src/components/blog/BlogSupport.tsx
+++ src/components/blog/BlogSupport.tsx
@@ -3,7 +3,12 @@ import LikeButton from '@/components/blog/LikeButton';
 export default function BlogSupport({ postId }: { postId: number }) {
   return (
     <div className="my-8 flex w-full items-center gap-2 rounded-xl border-2 border-zinc-500 p-8 dark:border-zinc-700">
-      <LikeButton postId={postId} />
+      <div className="flex flex-row items-center">
+        <LikeButton postId={postId} />
+        <p className="ml-2 text-lg text-zinc-500 dark:text-zinc-400">
+          Did you enjoy this post?
+        </p>
+      </div>
     </div>
   );
 }
