a005f7d96d2bdfce92a5ef427d53004b4608e801
amirardalan <hi@amir.sh>
2025-04-02 17:13:17 -0700

    Add avatar to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,12 +3,12 @@
 import PageHeading from '@/components/ui/PageHeading';
 import Container from '@/components/content/Container';
 import Tooltip from '@/components/ui/Tooltip';
-import IconWave from '@/components/icons/IconWave';
+import { CldImage } from 'next-cloudinary';
 
 export default function About() {
   return (
     <Container>
-      <main className="mt-8" aria-labelledby="about-heading">
+      <main className="mt-12" aria-labelledby="about-heading">
         <div id="about-heading">
           <PageHeading title={'About'} />
         </div>
@@ -27,77 +27,86 @@ export default function About() {
           </p>
         </section>
 
-        <div className="flex justify-center py-4" aria-hidden="true">
-          <IconWave />
-        </div>
-
-        <section className="mt-2 flex flex-col" aria-label="Contact links">
-          <div className="mb-2 flex flex-row items-center">
-            <p className="text-zinc-600 dark:text-zinc-500" id="x-link">
-              X:
-            </p>
-            <Tooltip text="Visit my X profile" pos="r">
-              <a
-                href="https://x.com/amirardalan"
-                rel="noopener noreferrer"
-                target="_blank"
-                className="ml-1 text-primary"
-                aria-labelledby="x-link"
-              >
-                @amirardalan
-              </a>
-            </Tooltip>
+        <section className="flex pt-10" aria-label="Contact links">
+          <div className="flex pb-4" aria-hidden="true">
+            <CldImage
+              src="About/amir-avatar_e6puqu"
+              alt="Amir Ardalan Avatar"
+              width={125}
+              height={125}
+              className="rounded-full"
+            />
           </div>
-          <div className="mb-2 flex flex-row items-center">
-            <p className="text-zinc-600 dark:text-zinc-500" id="github-link">
-              GitHub:
-            </p>
-            <Tooltip text="Visit my GitHub profile" pos="r">
-              <a
-                href="https://github.com/amirardalan"
-                rel="noopener noreferrer"
-                target="_blank"
-                className="ml-1 text-primary"
-                aria-labelledby="github-link"
+          <div className="flex flex-col justify-center pl-8">
+            <div className="mb-2 flex flex-row items-center">
+              <p className="text-zinc-600 dark:text-zinc-500" id="x-link">
+                X:
+              </p>
+              <Tooltip text="Visit my X profile" pos="r">
+                <a
+                  href="https://x.com/amirardalan"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="ml-1 text-primary"
+                  aria-labelledby="x-link"
+                >
+                  @amirardalan
+                </a>
+              </Tooltip>
+            </div>
+            <div className="mb-2 flex flex-row items-center">
+              <p className="text-zinc-600 dark:text-zinc-500" id="github-link">
+                GitHub:
+              </p>
+              <Tooltip text="Visit my GitHub profile" pos="r">
+                <a
+                  href="https://github.com/amirardalan"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="ml-1 text-primary"
+                  aria-labelledby="github-link"
+                >
+                  @amirardalan
+                </a>
+              </Tooltip>
+            </div>
+            <div className="mb-2 flex flex-row items-center">
+              <p
+                className="text-zinc-600 dark:text-zinc-500"
+                id="linkedin-link"
               >
-                @amirardalan
-              </a>
-            </Tooltip>
-          </div>
-          <div className="mb-2 flex flex-row items-center">
-            <p className="text-zinc-600 dark:text-zinc-500" id="linkedin-link">
-              LinkedIn:
-            </p>
-            <Tooltip text="Visit my LinkedIn profile" pos="r">
-              <a
-                href="https://linkedin.com/in/amirardalan"
-                rel="noopener noreferrer"
-                target="_blank"
-                className="ml-1 text-primary"
-                aria-labelledby="linkedin-link"
-              >
-                /in/amirardalan
-              </a>
-            </Tooltip>
-          </div>
-          <div className="mb-2 flex flex-row items-center">
-            <p className="text-zinc-600 dark:text-zinc-500" id="resume-link">
-              Resume:
-            </p>
-            <Tooltip text="Download my resume" pos="r">
-              <a
-                href="https://amir.sh/resume"
-                rel="noopener noreferrer"
-                target="_blank"
-                className="ml-1 text-primary"
-                aria-labelledby="resume-link"
-              >
-                download
-              </a>
-            </Tooltip>
+                LinkedIn:
+              </p>
+              <Tooltip text="Visit my LinkedIn profile" pos="r">
+                <a
+                  href="https://linkedin.com/in/amirardalan"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="ml-1 text-primary"
+                  aria-labelledby="linkedin-link"
+                >
+                  /in/amirardalan
+                </a>
+              </Tooltip>
+            </div>
+            <div className="mb-2 flex flex-row items-center">
+              <p className="text-zinc-600 dark:text-zinc-500" id="resume-link">
+                Resume:
+              </p>
+              <Tooltip text="Download my resume" pos="r">
+                <a
+                  href="https://amir.sh/resume"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="ml-1 text-primary"
+                  aria-labelledby="resume-link"
+                >
+                  download
+                </a>
+              </Tooltip>
+            </div>
           </div>
         </section>
-        {/* Remove the CldImage test */}
       </main>
     </Container>
   );
