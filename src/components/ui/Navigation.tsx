3663bf36fd71ca774cc3aafb4a9fe5474c15daf2
Amir Ardalan <hi@amirardalan.com>
2025-04-08 11:19:41 -0700

    Organize nav styles

--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -9,23 +9,24 @@ export default function Navigation({ header = false }: { header?: boolean }) {
 
   const getNavItemClass = (href: string, isLast?: boolean) => {
     return clsx({
+      // Header & Footer
       'border-b-2 pb-1': isActive(href),
-      'border-light text-light': isActive(href) && !header,
-      'border-dark text-dark': isActive(href) && header,
-      'dark:border-dark dark:text-dark': isActive(href) && !header,
-      'dark:border-light dark:text-light': isActive(href) && header,
-      'text-light': !isActive(href) && !header,
-      'text-dark': !isActive(href) && header,
-      'dark:text-dark': !isActive(href) && !header,
-      'dark:text-light': !isActive(href) && header,
+      // Header only
+      'mr-12 text-xxs uppercase': header,
+      'text-dark dark:text-light mr-12 text-xxs uppercase':
+        !isActive(href) && header,
+      'border-dark text-dark dark:border-light dark:text-light':
+        isActive(href) && header,
+      // Footer only
+      'text-light dark:text-dark': !isActive(href) && !header,
+      'border-light text-light dark:border-dark dark:text-dark':
+        isActive(href) && !header,
       'mr-6': !isLast && !header,
-      'mr-12': header,
-      'text-xxs uppercase': header,
     });
   };
 
   return (
-    <nav>
+    <nav className="text-light">
       <div className={clsx('flex flex-row')}>
         <Link href="/">
           <span className={getNavItemClass('/')}>Home</span>
