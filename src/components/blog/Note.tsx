c18c5eb8f9ec6bf739cc78de21ce8f1c07eb3b22
Amir Ardalan <hi@amirardalan.com>
2025-04-10 19:10:58 -0700

    Add blog note colors

--- src/components/blog/Note.tsx
+++ src/components/blog/Note.tsx
@@ -12,17 +12,31 @@ interface NoteProps {
 
 export default function Note({ children, noteType = 'note' }: NoteProps) {
   const noteConfig = {
-    note: { icon: <IconNote />, label: 'Note' },
-    tip: { icon: <IconTip />, label: 'Tip' },
-    warn: { icon: <IconWarn />, label: 'Warning' },
+    note: {
+      icon: <IconNote />,
+      label: 'Note',
+      color:
+        'text-blue-500 fill-blue-500 dark:text-blue-400 dark:fill-blue-400',
+    },
+    tip: {
+      icon: <IconTip />,
+      label: 'Tip',
+      color:
+        'text-amber-500 fill-amber-500 dark:text-amber-400 dark:fill-amber-400',
+    },
+    warn: {
+      icon: <IconWarn />,
+      label: 'Warning',
+      color: 'text-red-500 fill-red-500 dark:text-red-400 dark:fill-red-400',
+    },
   };
 
-  const { icon, label } = noteConfig[noteType];
+  const { icon, label, color } = noteConfig[noteType];
 
   return (
     <div className="note relative my-8 flex rounded-lg border border-zinc-300 dark:border-zinc-700">
       <div className="absolute left-4 top-2.5 text-xxs uppercase">
-        <div className="mt-1.5 flex items-center gap-1">
+        <div className={`mt-1.5 flex items-center gap-1 ${color}`}>
           <div className="h-4 w-4">{icon}</div>
           <div>{label}</div>
         </div>
