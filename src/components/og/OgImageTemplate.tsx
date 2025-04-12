907b8e72a7cef88bf2c16c0675501faf7c489cd3
Amir Ardalan <hi@amir.sh>
2025-04-12 16:53:38 -0700

    Improv og image styles

--- src/components/og/OgImageTemplate.tsx
+++ src/components/og/OgImageTemplate.tsx
@@ -1,7 +1,6 @@
 import { readFile } from 'node:fs/promises';
 import { join } from 'node:path';
 import { ImageResponse } from 'next/og';
-import Logo from '@/components/ui/Logo';
 
 export interface OgImageProps {
   title: string;
@@ -29,27 +28,34 @@ export async function generateOgImage({
   return new ImageResponse(
     (
       <div tw="flex h-full w-full bg-zinc-900">
-        <div tw="flex flex-1 flex-col p-12 relative">
+        <div tw="flex flex-col w-full h-full p-20 relative justify-between">
           {/* Site branding */}
           <div tw="flex w-full items-center justify-between">
-            <span tw="text-3xl text-zinc-400">amir.sh</span>
-            {tag && (
-              <span tw="text-3xl text-zinc-500 text-[#00FBFF]">#{tag}</span>
-            )}
+            <span tw="text-3xl text-[#00FBFF]">amir.sh</span>
+            {tag && <span tw="text-3xl text-zinc-500">#{tag}</span>}
           </div>
 
           <div tw="flex flex-col mt-12">
-            <h2 tw="flex flex-col text-7xl text-white text-left font-bold mt-4 w-full leading-none">
-              <span>{title}</span>
+            <h2 tw="text-7xl text-white text-left font-bold mt-4 w-full leading-none">
+              {title}
             </h2>
             <h3 tw="w-full text-5xl text-zinc-500 leading-none">
-              <span>{description}</span>
+              {description}
             </h3>
           </div>
 
           {/* Position the logo in the bottom right corner */}
-          <div tw="absolute bottom-12 right-12">
-            <Logo size={80} />
+          <div tw="flex items-center justify-end">
+            <svg
+              width={100}
+              height={100}
+              viewBox="0 0 286 286"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#00FBFF"
+            >
+              <path d="M211.825 42.6501L220.907 49.9753C245.665 69.9432 252.057 88.6088 240.081 105.972C236.179 111.724 230.662 117.475 223.531 123.227L130.284 198.432L74.4767 153.423L211.825 42.6501ZM93.2473 18.1514C108.116 6.15986 122.412 0.109808 136.137 0.00128681C151.207 -0.107234 167.186 6.64821 184.072 20.2676L193.155 27.5928L55.8071 138.366L0 93.3566L93.2473 18.1514Z" />
+              <path d="M92.845 258.487L101.928 265.813C126.686 285.781 149.829 290.935 171.358 281.277C178.49 278.13 185.621 273.68 192.753 267.929L286 192.724L230.193 147.715L92.845 258.487ZM62.469 162.853C47.6006 174.845 40.0991 186.375 39.9645 197.444C39.8299 209.599 48.2061 222.486 65.0928 236.105L74.1753 243.43L211.523 132.657L155.716 87.6481L62.469 162.853Z" />
+            </svg>
           </div>
         </div>
       </div>
