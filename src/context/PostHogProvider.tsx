333a0fe0814ab123728ed089acfe448dc6c91437
Amir Ardalan <hi@amirardalan.com>
2025-04-13 09:08:50 -0700

    Move posthog host to env var

--- src/context/PostHogProvider.tsx
+++ src/context/PostHogProvider.tsx
@@ -9,7 +9,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: '/ingest',
-      ui_host: 'https://us.posthog.com',
+      ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
       capture_pageview: false,
       capture_pageleave: true,
       loaded: function (ph) {
