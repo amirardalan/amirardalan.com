695c2d9633d13aa6727cf986fbe46925f1c58d17
Amir Ardalan <hi@amir.sh>
2025-04-12 22:08:03 -0700

    Remove env stub

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -1,18 +1,6 @@
 import { PostHog } from 'posthog-node';
 
 export default function PostHogClient() {
-  // Skip initialization in development
-  if (process.env.NODE_ENV === 'development') {
-    return {
-      capture: () => {},
-      identify: () => {},
-      groupIdentify: () => {},
-      isFeatureEnabled: () => false,
-      getFeatureFlag: () => null,
-      shutdownAsync: async () => {},
-    } as unknown as PostHog;
-  }
-
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     host: 'https://us.i.posthog.com',
     flushAt: 1,
