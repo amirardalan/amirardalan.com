480f6bd06d023aa8e8b54abb20d8b52e5e87db70
Amir Ardalan <hi@amirardalan.com>
2025-04-14 15:33:33 -0700

    Update important icon

--- src/components/icons/IconImportant.tsx
+++ src/components/icons/IconImportant.tsx
@@ -1,11 +1,18 @@
 export default function IconImportant() {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      clipRule="evenodd"
+      fillRule="evenodd"
+      strokeLinejoin="round"
+      strokeMiterlimit="2"
       viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
     >
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 15c-.55 0-1-.45-1-1v-4c0-.55.45-1 1-1s1 .45 1 1v4c0 .55-.45 1-1 1zm1-8h-2V7h2v2z" />
+      <path
+        d="m12.002 21.534c5.518 0 9.998-4.48 9.998-9.998s-4.48-9.997-9.998-9.997c-5.517 0-9.997 4.479-9.997 9.997s4.48 9.998 9.997 9.998zm0-8c-.414 0-.75-.336-.75-.75v-5.5c0-.414.336-.75.75-.75s.75.336.75.75v5.5c0 .414-.336.75-.75.75zm-.002 3c-.552 0-1-.448-1-1s.448-1 1-1 1 .448 1 1-.448 1-1 1z"
+        fill-rule="nonzero"
+      />
     </svg>
   );
 }
