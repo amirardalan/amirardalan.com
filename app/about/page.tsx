285af8718369ec7a206942b025d57f35494a33e1
Amir Ardalan <hi@amir.sh>
2025-04-14 16:52:45 -0700

    Improve about page styles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,7 +15,7 @@ export default function About() {
         <div id="about-heading">
           <PageHeading title={'About'} />
         </div>
-        <section className="mb-4 max-w-7xl font-serif text-xl font-normal italic md:text-2xl lg:mb-10 lg:text-4xl">
+        <section className="mb-4 max-w-7xl font-serif text-xl font-light italic md:text-2xl lg:mb-10 lg:text-4xl lg:leading-normal">
           <p className="mb-4 text-dark md:mb-6 dark:text-light">
             Fullstack Engineer based in Portland, OR.
           </p>
